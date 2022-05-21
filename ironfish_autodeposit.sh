9376d9580f899178e26d287996873a0ddd3c5066
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-05-21 12:56:03 +0300

    update ironfish deposit script

--- ironfish_autodeposit.sh
+++ ironfish_autodeposit.sh
@@ -1,9 +1,29 @@
+#i=0
+ 
+#while true
+#do
+#   date +"%T"
+#   docker exec ironfish ./bin/run deposit --confirm 
+#   let i++
+#   sleep 5m
+#done
+
 i=0
  
 while true
 do
-   date +"%T"
-   docker exec ironfish ./bin/run deposit --confirm 
-   let i++
-   sleep 5m
+
+   BALANCE=`docker exec ironfish ./bin/run  accounts:balance`
+   if [[ "$BALANCE" == *"IRON 0.0000"* ]]; then 
+      sleep 10s
+      echo "balance is 0"
+   else 
+      docker exec ironfish ./bin/run deposit --confirm
+      date +"%T"
+      sleep 10s
+      let i++
+      echo $BALANCE
+   fi 
+
 done
+
