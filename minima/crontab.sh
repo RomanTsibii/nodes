ab35782b247244b1bddbf0ab31d1c9b97b30e912
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-05-26 16:41:18 +0300

    Update crontab.sh

--- minima/crontab.sh
+++ minima/crontab.sh
@@ -1,6 +1,5 @@
-sleep 1
-history | grep "curl 127.0.0.1"
-sleep 1
+MINIMA_HISTORY=`history | grep "curl 127.0.0.1"`
+echo $MINIMA_HISTORY
 echo "Set comand for running every day at 9:00 am"
 echo "Example: curl 127.0.0.1:9002/incentivecash%20uid:7ef****d7-8141-4134-936d-2a0"
 read COMMAND
