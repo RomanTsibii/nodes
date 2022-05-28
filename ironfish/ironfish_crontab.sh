d5e811f058eb7ef4413be6a28529d72957fbdff8
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-05-28 12:13:11 +0300

    Update ironfish_crontab.sh

--- ironfish/ironfish_crontab.sh
+++ ironfish/ironfish_crontab.sh
@@ -1,12 +1,14 @@
-# cd ~ && curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/crontab.sh > ironfish_crontab.sh && chmod +x ironfish_crontab.sh && ./ironfish_crontab.sh
+# cd ~ && curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/ironfish/ironfish_crontab.sh > ironfish_crontab.sh && chmod +x ironfish_crontab.sh && ./ironfish_crontab.sh
 
 # file ironfish_restart_every_hour.sh
 
-STATUS=`docker exec ironfish ./bin/run status`
-if [[ "$STATUS" == *"SYNCING"* ]]; then 
+sudo bash -c 'cat << EOF > /root/ironfish_restart_every_hour.sh
+STATUS=\`docker exec ironfish ./bin/run status\`
+if [[ "\$STATUS" == *"SYNCING"* ]]; then 
   cd ~/
   docker-compose restart
 fi
+EOF'
 
 chmod +x ironfish_restart_every_hour.s
 (crontab -l 2>/dev/null || true; echo "5 * * * * /root/ironfish_restart_every_hour.sh") | crontab -
