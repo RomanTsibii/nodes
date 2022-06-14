333792fc3b59e54708a1d03a41238e783e3a0106
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-06-14 16:30:12 +0300

    Create rolls.sh

---
+++ massa/rolls.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# curl -s https://raw.githubusercontent.com/RomanTsibii/main/nodes/massa/rolls.sh > rolls.sh && chmod +x rolls.sh && tmux new-session -d -s rolls './rolls.sh'
+
+rm -f $HOME/massa/massa-client/massa-client
+if [ ! -e $HOME/massa/massa-client/massa-client ]; then
+  wget https://raw.githubusercontent.com/razumv/helpers/main/massa/massa-client -O $HOME/massa/massa-client/massa-client
+  chmod +x $HOME/massa/massa-client/massa-client
+fi
+#
+cd $HOME/massa/massa-client
+massa_wallet_address=$(./massa-client wallet_info | grep Address | awk '{ print $2 }')
+while true
+do
+        balance=$(./massa-client wallet_info | grep "Final balance" | awk '{ print $3 }')
+        int_balance=${balance%%.*}
+        if [ $int_balance -gt "99" ]; then
+                echo "More than 99"
+                resp=$(./massa-client buy_rolls $massa_wallet_address $(($int_balance/100)) 0)
+                echo $resp
+        elif [ $int_balance -lt "100" ]; then
+                echo "Less than 100"
+        fi
+        printf "sleep"
+        for((sec=0; sec<60; sec++))
+        do
+                printf "."
+                sleep 1
+        done
+        printf "\n"
+        if [[ `journalctl -n 5 -u massa` == *"Send network event failed An error occurred during channel communication: Failed to send event"*]]; then 
+                systemctl restart massa
+        end
+done
