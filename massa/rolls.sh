d14a1a286ecccf6267e3126f6d8dfab95c7ee8c5
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-06-15 02:21:25 +0300

    Update rolls.sh

--- massa/rolls.sh
+++ massa/rolls.sh
@@ -15,11 +15,11 @@ while true
 do
         balance=$(./massa-client wallet_info | grep "Final balance" | awk '{ print $3 }')
         int_balance=${balance%%.*}
-        if [ $int_balance -gt "99" ]; then
+        if [[ $int_balance -gt "99" ]]; then
                 echo "More than 99"
                 resp=$(./massa-client buy_rolls $massa_wallet_address $(($int_balance/100)) 0)
                 echo $resp
-        elif [ $int_balance -lt "100" ]; then
+        elif [[ $int_balance -lt "100" ]]; then
                 echo "Less than 100"
         fi
         printf "sleep"
