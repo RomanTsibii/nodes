f3dcd21d129c5f7f7db334b96962a0e9ccd43dde
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-07-07 00:27:10 +0300

    Create update_kysama.sh

---
+++ kyve/update_kysama.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+wget https://github.com/kyve-org/substrate/releases/download/v0.1.2/kyve-linux.zip
+unzip kyve-linux.zip 
+source $HOME/.profile
+rm -rf __MACOSX/ kyve-linux.zip 
+chmod u+x kyve-linux 
+mv kyve-linux /usr/bin/kyve-Kusama
+
+# запускаем ноду
+sudo systemctl daemon-reload && \
+sudo systemctl enable kyve-kysama && \
+sudo systemctl restart kyve-kysama
+  
+# проверяем логи
+sudo journalctl -u kyve-kysama -f -o cat
