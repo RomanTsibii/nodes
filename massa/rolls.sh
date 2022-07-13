f6ae1cb43a2f18db10975b2e0ec3817856c072fc
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-07-13 15:40:07 +0300

    Update rolls.sh

--- massa/rolls.sh
+++ massa/rolls.sh
@@ -10,17 +10,9 @@
 # fi
 #
 
-if [ ! $massa_discord_id ]; then
-  echo -e "Enter your discord id"
-  echo ' '
-  read massa_discord_id
-fi
-
+source .profile
 cd $HOME/massa/massa-client
-
 massa_wallet_address=$(./massa-client -p $massa_pass wallet_info | grep Address | awk '{ print $2 }')
-./massa-client -p $massa_pass node_testnet_rewards_program_ownership_proof $massa_wallet_address $massa_discord_id
-
 
 while true
 do
