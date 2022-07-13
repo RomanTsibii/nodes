e58aa3443e534b3f4a3014e6b8cc61df932f35f8
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-07-13 21:58:07 +0300

    Update crontab.sh

--- minima/crontab.sh
+++ minima/crontab.sh
@@ -5,8 +5,8 @@
 history | grep "curl 127.0.0.1"
 sleep 1
 echo "Set comand for running every day at 9:00 am"
-echo "Example: 7ef****d7-8141-4134-936d-2a0"
+echo "Example: curl 127.0.0.1:9002/incentivecash%20uid:7ef****d7-8141-4134-936d-2a0"
 read COMMAND
-echo "curl 127.0.0.1:9002/incentivecash%20uid:$COMMAND" >> minima_autorun_every_day.sh
+echo $COMMAND >> minima_autorun_every_day.sh
 chmod u+x minima_autorun_every_day.sh
 (crontab -l 2>/dev/null || true; echo "* 9 * * * /root/minima_autorun_every_day.sh") | crontab -
