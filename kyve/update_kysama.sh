c5c69dc998f4414215bba182c3eb000692ebeaf5
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-07-14 20:08:51 +0300

    Update update_kysama.sh

--- kyve/update_kysama.sh
+++ kyve/update_kysama.sh
@@ -2,7 +2,9 @@
 
 # bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/kyve/update_kysama.sh)
 
-wget https://github.com/kyve-org/substrate/releases/download/v0.3.1/kyve-linux.zip
+
+# wget https://github.com/kyve-org/substrate/releases/download/v0.3.1/kyve-linux.zip
+wget https://github.com/kyve-org/substrate/releases/download/v0.3.0/kyve-linux.zip
 unzip kyve-linux.zip 
 source $HOME/.profile
 rm -rf __MACOSX/ kyve-linux.zip 
