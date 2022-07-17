e3de83e595ae56523566fdf8d814f70fd6e7abb4
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-07-18 00:21:34 +0300

    Create restart.sh

---
+++ minima/restart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+systemctl restart minima_9001
+systemctl restart minima_9003
+systemctl restart minima_9005
+systemctl restart minima_9007
+systemctl restart minima_9009
+systemctl restart minima_9011
+systemctl restart minima_9013
+systemctl restart minima_9015
