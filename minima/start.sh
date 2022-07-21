4d04dddb7c5316d5f703dea5667add8358a0bc63
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-07-21 18:48:01 +0300

    Update start.sh

--- minima/start.sh
+++ minima/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/restart.sh)
+# bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/start.sh)
 
 pkill -9 /usr/bin/java
 echo 'kill /usr/bin/java'
