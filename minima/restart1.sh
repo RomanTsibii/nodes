ff0f690504fdc941015e92ee18cb04718ed8803d
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-07-23 10:28:12 +0300

    Update restart1.sh

--- minima/restart1.sh
+++ minima/restart1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/restart.sh)
+# bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/restart1.sh)
 
 source $HOME/.profile
 pkill -9 /usr/bin/java
