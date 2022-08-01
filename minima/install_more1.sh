4c24f778a4efe5f547a138480b55ef14e1697661
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-08-01 09:24:56 +0300

    Create install_more1.sh

---
+++ minima/install_more1.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9002 -p 9001
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9004 -p 9003
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9006 -p 9005
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9008 -p 9007
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9010 -p 9009
+echo "install more is DONE"
