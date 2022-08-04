75d0dd9746dbacb522bcdd60b16d621bad7fc731
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-08-04 21:47:55 +0300

    Create many_installling.sh

---
+++ minima/many_installling.sh
@@ -0,0 +1,129 @@
+#!/bin/sh
+# bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/many_installling.sh)
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9044 -p 9043
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9046 -p 9045
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9048 -p 9047
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9050 -p 9049
+sleep 15
+systemctl stop minima_90*
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9052 -p 9051
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9054 -p 9053
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9056 -p 9055
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9058 -p 9057
+sleep 15
+systemctl stop minima_90*
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9060 -p 9059
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9062 -p 9061
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9064 -p 9063
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9066 -p 9065
+sleep 15
+systemctl stop minima_90*
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9068 -p 9067
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9070 -p 9069
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9072 -p 9071
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9074 -p 9073
+sleep 15
+systemctl stop minima_90*
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9076 -p 9075
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9078 -p 9077
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9080 -p 9079
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9082 -p 9081
+sleep 15
+systemctl stop minima_90*
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9084 -p 9083
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9086 -p 9085
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9088 -p 9087
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 3502 -p 3501
+sleep 15
+systemctl stop minima_90*
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 3504 -p 3503
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 3506 -p 3505
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 3508 -p 3507
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 3510 -p 3509
+sleep 15
+systemctl stop minima_90*
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 3512 -p 3511
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 3514 -p 3513
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 3516 -p 3515
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 3518 -p 3517
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 3520 -p 3519
+sleep 15
+systemctl stop minima_90*
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 3522 -p 3521
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 3524 -p 3523
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 3526 -p 3525
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 3528 -p 3527
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 3530 -p 3529
+sleep 15
+
+wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 3532 -p 3531
+sleep 15
+systemctl stop minima_90*
+
