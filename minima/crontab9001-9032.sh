7a8572733841a36c1e7717461450a2968385a5ce
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-08-06 11:53:30 +0300

    Create crontab9001-9032.sh

---
+++ minima/crontab9001-9032.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# cd ~ && curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/crontab9001-9032.sh > crontab9001-9032.sh && chmod +x crontab9001-9032.sh && ./crontab9001-9032.sh
+
+touch minima_reping_base.sh
+chmod +x minima_reping_base.sh
+
+cat << EOF > /root/minima_reping_base.sh
+#!/bin/bash
+bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/ping_base_all.sh)
+EOF
+
+
+(crontab -l 2>/dev/null || true; echo "0 1 * * * /root/minima_reping_base.sh") | crontab -
+(crontab -l 2>/dev/null || true; echo "0 3 * * * /root/minima_reping_base.sh") | crontab -
+(crontab -l 2>/dev/null || true; echo "0 5 * * * /root/minima_reping_base.sh") | crontab -
+(crontab -l 2>/dev/null || true; echo "0 7 * * * /root/minima_reping_base.sh") | crontab -
+rm crontab9001-9032.sh
