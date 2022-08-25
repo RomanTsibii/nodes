f31d581ff512633f971173573e184cde603dbba6
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-08-25 10:06:22 +0300

    Update aptos_get_data.sh

--- aptos/aptos_get_data.sh
+++ aptos/aptos_get_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/aptos/aptos_get_data.sh)
 
 source $HOME/.bash_profile
-cat $HOME/aptos_testnet/$aptos_username.yaml
+cat ~/aptos_testnet/keys/public-keys.yaml
