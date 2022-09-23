9a916d9535baca693ee413280d6a7ea72ff5c92f
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-09-23 12:25:46 +0300

    Update rolls.sh

--- massa/rolls.sh
+++ massa/rolls.sh
@@ -3,20 +3,15 @@
 # pkill -9 tmux 
 # curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/massa/rolls.sh > rolls.sh && chmod +x rolls.sh && tmux new-session -d -s rolls './rolls.sh'
 
-rm -f $HOME/massa/massa-client/massa-client
-if [ ! -e $HOME/massa/massa-client/massa-client ]; then
-  wget https://raw.githubusercontent.com/razumv/helpers/main/massa/massa-client -O $HOME/massa/massa-client/massa-client
-  chmod +x $HOME/massa/massa-client/massa-client
-fi
-
-
 source $HOME/.profile
 cd $HOME/massa/massa-client
-massa_wallet_address=$(./massa-client -p $massa_pass wallet_info | grep Address | awk '{ print $2 }')
 
+massa_wallet_address=$(./massa-client -p $massa_pass wallet_info | grep Address | awk '{ print $2 }')
+./massa-client -p $massa_pass node_add_staking_secret_keys $(./massa-client -p $massa_pass wallet_info | grep Secret | awk '{ print $3 }')
 while true
 do
-        balance=$(./massa-client -p $massa_pass wallet_info | grep "Final balance" | awk '{ print $3 }')
+        balance1=$(./massa-client -p $massa_pass wallet_info | grep Sequential | awk '{ print $4 }')
+        balance=${balance1:10}
         int_balance=${balance%%.*}
         if [[ $int_balance -gt "99" ]]; then
                 echo "More than 99"
@@ -26,32 +21,32 @@ do
                 echo "Less than 100"
         fi
 
-         massa_logs=`journalctl -n 1 -u massa`
-         if [[ $massa_logs == *"Send network event failed An error occurred during channel communication: Failed to send event"* ]]; then 
-                 echo 'Restarting...'
-                 systemctl restart massa
-         fi
+#          massa_logs=`journalctl -n 1 -u massa`
+#          if [[ $massa_logs == *"Send network event failed An error occurred during channel communication: Failed to send event"* ]]; then 
+#                  echo 'Restarting...'
+#                  systemctl restart massa
+#          fi
 #         if [[ $massa_logs == *"speculative execution cache empty, executing final slot"* ]]; then 
 #                 echo 'Restarting...'
 #                 systemctl restart massa
 #         fi
-        
+#         
 #         massa_logs=`journalctl -n 30 -u massa`
 #         if [[ $massa_logs == *"RUST_BACKTRACE=1"* ]]; then 
 #                 echo 'Updating massa'
 #                 RUST_BACKTRACE=1
 #                 bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/massa/update_11_3.sh)
 #         fi
-        
-        staking_registered_address=$(./massa-client  -p $massa_pass node_get_staking_addresses)
-        staking_address=$(./massa-client  -p $massa_pass wallet_info | grep 'Address' | cut -d\   -f2)
-        
-        if [ "$staking_registered_address" = "$staking_address" ]; then
-               echo "Node was registered"
-        else
-               echo "Node wasn't registered"
-               ./massa-client -p $massa_pass node_add_staking_secret_keys $(./massa-client  -p $massa_pass wallet_info | grep 'Secret key' | cut -d\    -f3)
-        fi
+#         
+#         staking_registered_address=$(./massa-client  -p $massa_pass node_get_staking_addresses)
+#         staking_address=$(./massa-client  -p $massa_pass wallet_info | grep 'Address' | cut -d\   -f2)
+#         
+#         if [ "$staking_registered_address" = "$staking_address" ]; then
+#                echo "Node was registered"
+#         else
+#                echo "Node wasn't registered"
+#                ./massa-client -p $massa_pass node_add_staking_secret_keys $(./massa-client  -p $massa_pass wallet_info | grep 'Secret key' | cut -d\    -f3)
+#         fi
         
         printf "sleep"
         for((sec=0; sec<60; sec++))
