7357179f2e4379ca044abb95034cb1279b9eaea9
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-09-26 22:39:08 +0300

    Update crontab9001-9032.sh

--- minima/crontab9001-9032.sh
+++ minima/crontab9001-9032.sh
@@ -2,6 +2,13 @@
 
 # cd ~ && curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/crontab9001-9032.sh > crontab9001-9032.sh && chmod +x crontab9001-9032.sh && ./crontab9001-9032.sh
 
+
+echo "Set comand for running every day at 9:00 am"
+echo "Example: curl 127.0.0.1:9002/incentivecash%20uid:7ef****d7-8141-4134-936d-2a0"
+read COMMAND
+echo $COMMAND >> minima_autorun_every_day.sh
+chmod u+x minima_autorun_every_day.sh
+
 touch minima_reping_base.sh
 chmod +x minima_reping_base.sh
 
