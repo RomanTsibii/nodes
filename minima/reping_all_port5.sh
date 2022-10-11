b3813d373f365e7e40ffa7bca562c88c269b6513
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-10-11 22:41:15 +0300

    Create reping_all_port5.sh

---
+++ minima/reping_all_port5.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/reping_all_port5.sh)
+
+source $HOME/.profile
+pkill -9 /usr/bin/java
+
+function some_speeping {
+  echo 'sleep'
+  for((sec=0; sec<100; sec++))
+  do
+          printf "."
+          sleep 1
+  done
+}
+
+function restart {
+  for PORT in ${PORTS} ; do
+    echo "restart minima_${PORT}"
+    systemctl restart minima_${PORT}
+  done
+}
+
+function ping {
+  for PORT in ${PORTS} ; do
+    echo "ping minima_${PORT}"
+    $(`cat minima_autorun_every_day.sh | grep $((PORT+4))`)
+  done
+}
+
+function stop {
+  echo "stop minima_${PORTS}"
+  systemctl stop minima_90*
+  systemctl stop minima_35*
+}
+
+
+function ping0 {
+  PORTS="9001 9003 9005 9007"
+  restart
+  some_speeping
+  ping
+  stop
+}
+
+function ping1 {
+  PORTS="9009 9011 9013 9015"
+  restart
+  some_speeping
+  ping
+  stop
+}
+
+function ping2 {
+  PORTS="9017 9019 9021 9023"
+  restart
+  some_speeping
+  ping
+  stop
+}
+
+function ping3 {
+  PORTS="9025 9027 9029 9031"
+  restart
+  some_speeping
+  ping
+  stop
+}
+
+function remove_all_database {
+  echo 'remove minima database'
+  rm -rf /home/minima/.minima_*
+}
+
+# remove_all_database
+
+ping0
+ping1
+ping2
+ping3
+ping0
+
+echo DONE
