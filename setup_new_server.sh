a4189f4a193826b5982f69075ac319e93310fd69
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-10-14 10:15:30 +0300

    Update setup_new_server.sh

--- setup_new_server.sh
+++ setup_new_server.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/setup_new_server.sh)
+
 # оновити час
+echo "set Ukraine timezone"
+sudo timedatectl set-timezone Europe/Kiev
 # встановити докер
+
 # встановити tmux
 # встановити ncdu
+sudo apt install ncdu tmux -y
