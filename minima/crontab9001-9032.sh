597c2dcdb7e34a52459ce2beca5233c285c25bfa
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-10-14 18:13:10 +0300

    Update crontab9001-9032.sh

--- minima/crontab9001-9032.sh
+++ minima/crontab9001-9032.sh
@@ -14,7 +14,7 @@ chmod +x minima_reping_base.sh
 
 cat << EOF > /root/minima_reping_base.sh
 #!/bin/bash
-bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/ping_base_all.sh)
+bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/reping_all_port5.sh)
 EOF
 
 
