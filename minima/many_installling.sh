5e1b600e30e298697e37d26999415791128642d4
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-10-14 17:28:51 +0300

    Update many_installling.sh

--- minima/many_installling.sh
+++ minima/many_installling.sh
@@ -4,7 +4,7 @@
 PORTS="9001 9003 9005 9007 9009 9011 9013 9015 9017 9019 "
 for PORT in ${PORTS} ; do
   echo "ping minima_${PORT}"
-  wget -O minima_setup.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r $((PORT+1)) -p ${PORT}
+  bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh) -p $PORT
   systemctl disable minima_${PORT}
   systemctl stop minima_${PORT}
 done
