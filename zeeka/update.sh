c9b0817047bd10690c28c345ff46f9adf31bf087
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-10-24 02:52:04 +0300

    Update update.sh

--- zeeka/update.sh
+++ zeeka/update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/zeeka/update.sh)
 
 systemctl stop bazuka
@@ -8,8 +7,6 @@ git pull origin master
 cargo update 
 cargo install --path .
 
-
-
 rm -rf ~/.bazuka ~/.bazuka-chaos
 cd 
 bazuka init --seed '"$BAZUKA_KEY"' --network chaos --node 127.0.0.1:8765
@@ -17,7 +14,10 @@ bazuka init --seed '"$BAZUKA_KEY"' --network chaos --node 127.0.0.1:8765
 
 sudo systemctl daemon-reload
 sudo systemctl enable bazuka
-
 sudo systemctl restart bazuka
 
-sudo journalctl -f -u bazuka
+echo "DONE"
+
+echo "sudo journalctl -f -u bazuka"
+echo "bazyka status"
+echo "bazuka wallet"
