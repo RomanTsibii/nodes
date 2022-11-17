117e5e74dfff7db60372851252e83b31da20911c
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-11-17 22:04:43 +0200

    Update update_reping_file.sh

--- minima/update_reping_file.sh
+++ minima/update_reping_file.sh
@@ -3,5 +3,5 @@
 
 cat << EOF > /root/minima_reping_base.sh
 #!/bin/bash
-bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/many_remove_and_install.sh)
+bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/many_remove_and_install2.sh)
 EOF
