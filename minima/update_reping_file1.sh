507d0827a601f9fc67403c1868475b3ab59a997f
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-11-17 22:09:14 +0200

    Update and rename update_reping_file.sh to update_reping_file1.sh

--- minima/update_reping_file1.sh
+++ minima/update_reping_file1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/update_reping_file.sh)
+# bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/update_reping_file1.sh)
 
 cat << EOF > /root/minima_reping_base.sh
 #!/bin/bash
