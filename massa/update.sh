7cf0644cc5dd5e09156b791fd46024091a9a4b14
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-12-03 11:09:07 +0200

    Update update.sh

--- massa/update.sh
+++ massa/update.sh
@@ -40,7 +40,7 @@ function delete {
 
 function install {
   wget https://github.com/massalabs/massa/releases/download/TEST.17.1/massa_TEST.17.1_release_linux.tar.gz
-  tar zxvf massa_TEST.17.0_release_linux.tar.gz -C $HOME/
+  tar zxvf massa_TEST.17.1_release_linux.tar.gz -C $HOME/
 }
 
 function routable_ip {
