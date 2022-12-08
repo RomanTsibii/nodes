131e39ffd1c30820d2b9283fc64f9128df55c1d2
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-12-08 13:18:50 +0200

    Update update.sh

--- starknet/update.sh
+++ starknet/update.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/starknet/update.sh)
+
 cd $HOME/pathfinder
 docker-compose down
 docker-compose pull
 docker-compose up -d
+
+docker-compose -f $HOME/pathfinder/docker-compose.yml logs -f --tail=10
