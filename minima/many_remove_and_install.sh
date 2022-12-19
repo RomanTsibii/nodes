ed408aa3caab0567e5016a48ecb0f0d070e0a6c0
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-12-20 00:09:26 +0200

    Update many_remove_and_install.sh

--- minima/many_remove_and_install.sh
+++ minima/many_remove_and_install.sh
@@ -5,9 +5,10 @@
 # apt install openjdk-11-jre-headless curl jq -y
 
 
-PORTS="9001 9003 9005 9007 9009 9011 9013 9015 9017 9019 9021 9023 9025 9027 9029 9031"
+PORTS="9003 9005 9007 9009 9011 9013 9015 9017 9019 9021 9023 9025 9027 9029 9031"
 echo $PORTS
 sudo apt update
+systemctl stop minima_9001
 for PORT in ${PORTS}
   do   
   systemctl stop minima_90*
@@ -28,4 +29,9 @@ for PORT in ${PORTS}
   systemctl stop minima_$PORT
   bash <(curl -s https://raw.githubusercontent.com/minima-global/Minima/master/scripts/minima_remove.sh) -p $PORT
 done
+
+systemctl restart minima_9001
+bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh) -p 9001
+sleep 80
+$(`cat minima_autorun_every_day.sh | grep 9005
 
