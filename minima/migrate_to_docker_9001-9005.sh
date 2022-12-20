ee1d98198957b9ebdb7446dae091f0b9418fe2c8
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-12-20 22:37:51 +0200

    Update migrate_to_docker_9001-9005.sh

--- minima/migrate_to_docker_9001-9005.sh
+++ minima/migrate_to_docker_9001-9005.sh
@@ -5,6 +5,9 @@ bash <(curl -s https://raw.githubusercontent.com/minima-global/Minima/master/scr
 bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh)
 MINIMA_PASSWORD=bnbiminima
 docker run -d -e minima_mdspassword=$MINIMA_PASSWORD -e minima_server=true -v ~/minimadocker9001:/home/minima/data -p 9001-9004:9001-9004 --restart unless-stopped --name minima9001 minimaglobal/minima:latest
+sleep 5
+docker run -d --restart unless-stopped --name watchtower -e WATCHTOWER_CLEANUP=true -e WATCHTOWER_TIMEOUT=60s -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
+sleep 10 
 MINIMA_UID="$(cut -d':' -f3 <<<"$(``cat minima_autorun_every_day.sh | grep 9005``)")"
 docker exec -d minima9001 sh -c "(sleep 5; echo 'incentivecash uid:$MINIMA_UID'; sleep 5; echo 'exit') | java -cp /usr/local/minima/minima.jar org.minima.utils.MinimaRPCClient"
 
