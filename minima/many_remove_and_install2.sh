51dff44ed6a65fc7a53e7dc42791f84b3c142697
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-12-20 23:11:43 +0200

    Update many_remove_and_install2.sh

--- minima/many_remove_and_install2.sh
+++ minima/many_remove_and_install2.sh
@@ -4,7 +4,7 @@
 PORTS="9003 9005 9007 9009 9011 9013 9015 9017 9019 9021 9023 9025 9027 9029 9031 9033 9035 9037 9039 9041 9043 9045 9047 9049 9051 9053 9055 9057 9059 9061 9063 9065 9067 9069 9071 9073 9075 9077 9079 9081 9083 9085 9087 3501 3503 3505 3507 3509 3511 3513 3515 3517 3519 3521 3523 3525 3527 3529 3531"
 echo $PORTS
 sudo apt update
-systemctl stop minima_9001
+docker stop  minima9001
 for PORT in ${PORTS}
   do   
   systemctl stop minima_90*
@@ -25,7 +25,4 @@ for PORT in ${PORTS}
   bash <(curl -s https://raw.githubusercontent.com/minima-global/Minima/master/scripts/minima_remove.sh) -p $PORT
 done
 
-systemctl restart minima_9001
-bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/minima_setup.sh) -p 9001
-sleep 80
-$(`cat minima_autorun_every_day.sh | grep 9005`)
+docker start minima9001
