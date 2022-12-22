d10f6717d415124f74844dbcd1300a06f16a60dc
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-12-22 17:59:35 +0200

    Create namada_gen_wallet.sh

---
+++ namada/namada_gen_wallet.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/namada/namada_install.sh)
+
+source $HOME/.profile
+cd $HOME
+namada wallet address gen --alias wallet_name
+#enter pass
+namada client init-validator --alias $VALIDATOR_ALIAS --source wallet_name --commission-rate 0.05 --max-commission-rate-change 0.01
+#enter pass
+
+cd $HOME
+namadac transfer \
+    --token NAM \
+    --amount 1000 \
+    --source faucet \
+    --target $VALIDATOR_ALIAS \
+    --signer $VALIDATOR_ALIAS
+	
+#use faucet again because min stake 1000 and you need some more NAM
+namadac transfer \
+    --token NAM \
+    --amount 1000 \
+    --source faucet \
+    --target $VALIDATOR_ALIAS \
+    --signer $VALIDATOR_ALIAS
+	
+#check balance
+namada client balance --owner $VALIDATOR_ALIAS --token NAM
+
+#stake your funds
+namada client bond \
+  --validator $VALIDATOR_ALIAS \
+  --amount 1500
+  
