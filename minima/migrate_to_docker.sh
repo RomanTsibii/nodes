5cbc129f40621ab16ed83661ec6e471c87febabf
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-12-23 22:43:34 +0200

    Update migrate_to_docker.sh

--- minima/migrate_to_docker.sh
+++ minima/migrate_to_docker.sh
@@ -9,13 +9,13 @@ function update {
   echo $PORT
   echo $PORT4
   
-  bash <(curl -s https://raw.githubusercontent.com/minima-global/Minima/master/scripts/minima_remove.sh) -p $PORT -x
+  bash <(curl -s https://raw.githubusercontent.com/minima-global/Minima/master/scripts/minima_remove.sh) -p $PORT_MAIN -x
  
   MINIMA_PASSWORD=bnbiminima
+#  docker run -d -e minima_mdspassword=$MINIMA_PASSWORD -e minima_server=true -v ~/minimadocker19001:/home/minima/data -p 19001-19004:9001-9004 --restart unless-stopped --name minima19001 minimaglobal/minima:latest
   docker run -d -e minima_mdspassword=$MINIMA_PASSWORD -e minima_server=true -v ~/minimadocker$PORT:/home/minima/data -p $PORT-$PORT4:9001-9004 --restart unless-stopped --name minima$PORT minimaglobal/minima:latest
-  sleep 5
-  docker run -d --restart unless-stopped --name watchtower -e WATCHTOWER_CLEANUP=true -e WATCHTOWER_TIMEOUT=60s -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
-  sleep 10 
+  sleep 15
+#  sleep 10 
   MINIMA_UID="$(cut -d':' -f3 <<<"$(``cat minima_autorun_every_day.sh | grep $PORT_MAIN``)")"
   docker exec -d minima$PORT sh -c "(sleep 5; echo 'incentivecash uid:$MINIMA_UID'; sleep 5; echo 'exit') | java -cp /usr/local/minima/minima.jar org.minima.utils.MinimaRPCClient"
 
@@ -27,7 +27,7 @@ function ports01 {
   do
     PRE=${PORT_MAIN:(-2)}
     PORT="482$PRE"
-    PORT4=$((PORT+4))
+    PORT4=$((PORT+3))
     update
   done
 }
