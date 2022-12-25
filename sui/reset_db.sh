7927451eb202727129946a4de08a8704fd4e1f2f
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-12-25 12:41:07 +0200

    Update and rename update.sh to reset_db.sh

--- sui/reset_db.sh
+++ sui/reset_db.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/sui/update.sh)
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/sui/reset_db.sh)
 
 
 systemctl stop suid; \
 rm -rf $HOME/.sui/db; \
 wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob; \
 systemctl restart suid
-echo DONE
+echo "reset_db DONE"
