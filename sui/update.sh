c753fc4c295317b9bb9abfbc528b038df5d09290
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2022-12-25 12:22:32 +0200

    Create update.sh

---
+++ sui/update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/sui/update.sh)
+
+
+systemctl stop suid; \
+rm -rf $HOME/.sui/db; \
+wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob; \
+systemctl restart suid
