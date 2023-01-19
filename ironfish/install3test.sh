dc70a424ee668a04d8859eb9f6d5d941d355aa3f
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-01-19 14:10:06 +0200

    Update install3test.sh

--- ironfish/install3test.sh
+++ ironfish/install3test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/minima/ping_all2.sh)
 
 curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_docker.sh | bash
 
