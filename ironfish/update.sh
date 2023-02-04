7cc899426f48eb3692aea933cad42cd753d53c9f
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-02-05 00:52:13 +0200

    Update update.sh

--- ironfish/update.sh
+++ ironfish/update.sh
@@ -4,6 +4,7 @@
 
 docker-compose down
 docker-compose pull
+wget -O $HOME/.ironfish/hosts.json https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/ironfish/hosts.json
 docker-compose run --rm --entrypoint "./bin/run migrations:start" ironfish
 docker-compose up -d
 
