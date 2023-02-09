b4421ffef9e7a2eddc9d18ac040fe7124c361cc5
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-02-09 14:42:37 +0200

    Update send_burn_mint.sh

--- ironfish/send_burn_mint.sh
+++ ironfish/send_burn_mint.sh
@@ -14,16 +14,16 @@ function sleep_seconds () {
 IRONFISH_NODENAME=`docker exec ironfish ./bin/run config | grep nodeName | awk '{print $2}' | sed 's/\"//g' |  tr ',' ' '`
 
 # mint
-docker exec ironfish ./bin/run  wallet:mint -a 1000 -f $IRONFISH_NODENAME -m $IRONFISH_NODENAME -n $IRONFISH_NODENAME -o 0.00000001 --confirm
+docker exec ironfish ./bin/run  wallet:mint -a 1000 -f $IRONFISH_NODENAME -m $IRONFISH_NODENAME -n $IRONFISH_NODENAME -o 0.00001 --confirm
 echo "sleep 500 sec"
 sleep_seconds 5400
 
 # Burn
 ASSET=`docker exec ironfish ./bin/run wallet:balances | grep " $IRONFISH_NODENAME" | awk '{print $2}'`
-docker exec ironfish ./bin/run wallet:burn -a 500 -f $IRONFISH_NODENAME -i $ASSET -o 0.00000001 --confirm
+docker exec ironfish ./bin/run wallet:burn -a 500 -f $IRONFISH_NODENAME -i $ASSET -o 0.00001 --confirm
 echo "sleep 500 sec"
 sleep_seconds 5400
 
 # Send
-docker exec ironfish ./bin/run wallet:send -a 250 -f $IRONFISH_NODENAME -i $ASSET -t dfc2679369551e64e3950e06a88e68466e813c63b100283520045925adbe59ca -o 0.00000001 --confirm
+docker exec ironfish ./bin/run wallet:send -a 250 -f $IRONFISH_NODENAME -i $ASSET -t dfc2679369551e64e3950e06a88e68466e813c63b100283520045925adbe59ca -o 0.00001 --confirm
 
