6874ee07f3e8f23760001f318d4244b7b34eef60
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-02-11 21:07:48 +0200

    Update setup_new_server.sh

--- setup_new_server.sh
+++ setup_new_server.sh
@@ -12,8 +12,8 @@ bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh
 sudo apt install ncdu tmux htop screen -y
 
 # for minima
-apt install openjdk-11-jre-headless curl jq -y
-
+apt install openjdk-11-jre-headless curl jq speedtest-cli -y
+ 
 # install proxy for adspower
 bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/helper/proxy_for_antick.sh)
 
