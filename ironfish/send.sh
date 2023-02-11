269d412aa4f74959f5504ae59aa9405f00aa1b5f
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-02-12 01:15:36 +0200

    Update send.sh

--- ironfish/send.sh
+++ ironfish/send.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 # bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/ironfish/send.sh)
 
+function sleep_seconds () {
+  for((sec=0; sec<"$1"; sec++))
+    do
+      printf "."
+      sleep 1
+    done
+    echo " "
+  }
+  
 WALLETS="
-7b036c6f22b9d0397c27c448cdeb07a33a5f9273a07305dac808d12f23ec0e06
-b42fc78b6fb8eeeb629d138df332c72b4fa15d5885d0dc1c0afac33b9becc2a5
-d7c86706f5817aa718cd1cfad03233bcd64a7789fd9422d3b17af6823a7e6ac6
+485b12f3dd3c14611c17820175aa75121ec2776905d6f8e2af27c7e2924980ae
+ff7f4bcacdfaa97656cd350b012bfb6ed860dc3e6660577b13bf43affd3a5342
   "
 
 for WALLET in ${WALLETS} ; do
   docker exec ironfish ./bin/run wallet:send -a 0.018 -i d7c86706f5817aa718cd1cfad03233bcd64a7789fd9422d3b17af6823a7e6ac6 -t $WALLET -o 0.001 --confirm
-    sleep 300
+    sleep_seconds 300
 done
 
 
