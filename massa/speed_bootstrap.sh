77986970f365770eb5f17fdecf683239c604b97a
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-02-15 11:58:49 +0200

    Update speed_bootstrap.sh

--- massa/speed_bootstrap.sh
+++ massa/speed_bootstrap.sh
@@ -1,6 +1,11 @@
 #/bin/bash
 # bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/massa/speed_bootstrap.sh)
 
+sed -i '/alias client/d' $HOME/.profile
+source $HOME/.profile
+echo "alias client='cd $HOME/massa/massa-client/ && $HOME/massa/massa-client/massa-client --pwd $massa_pass && cd'" >> ~/.profile
+echo "alias clientw='cd $HOME/massa/massa-client/ && $HOME/massa/massa-client/massa-client --pwd $massa_pass && cd'" >> ~/.profile
+
 config_path="$HOME/massa/massa-node/base_config/config.toml"
 sed -i -e "s%retry_delay *=.*%retry_delay = 15000%; " "$config_path"
 sudo systemctl restart massa
