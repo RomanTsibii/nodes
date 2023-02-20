2aed00c37cb68c4844d3dd2ac08a493623156730
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-02-20 09:28:16 +0200

    Update show_rolls.sh

--- massa/show_rolls.sh
+++ massa/show_rolls.sh
@@ -3,7 +3,7 @@
 
 source $HOME/.profile
 cd $HOME/massa/massa-client
-balance=$(./massa-client --pwd $massa_pass wallet_info | grep "Balance" |)
+balance=$(./massa-client --pwd $massa_pass wallet_info | grep "Balance")
 echo $balance
 candidate_rolls=$(./massa-client --pwd $massa_pass wallet_info | grep "Rolls")
 echo $candidate_rolls
