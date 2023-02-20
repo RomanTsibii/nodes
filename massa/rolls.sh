8dcb6a9ea7e14bcf532f7c190f9a84f8cdf51ea7
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-02-20 09:35:50 +0200

    Update rolls.sh

--- massa/rolls.sh
+++ massa/rolls.sh
@@ -3,13 +3,51 @@
 # pkill -9 tmux 
 # curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/massa/rolls.sh > rolls.sh && chmod +x rolls.sh && tmux new-session -d -s rolls './rolls.sh'
 
+
+
+cd $HOME/massa/massa-client
+massa_wallet_address=$(./massa-client --pwd $massa_pass wallet_info | grep Address | awk '{ print $2 }')
+./massa-client -p $massa_pass node_start_staking  $massa_wallet_address
+
+
+while true
+do
+        balance=$(./massa-client --pwd $massa_pass wallet_info | grep "Rolls" | awk '{ print $3 }' | sed 's/final=//;s/,//')
+        int_balance=${balance%%.*}
+        if [ $int_balance -lt "1" ]; then
+                echo "Less than 1 Final Roll"
+                resp=$(./massa-client --pwd $massa_pass buy_rolls $massa_wallet_address 1 0)
+                echo $resp
+        elif [ $int_balance -gt "1" ]; then
+                echo "More than 1 Final Roll"
+        fi
+        date=$(date +"%H:%M")
+        echo Last Update: ${date}
+        printf "sleep"
+        for((m=0; m<60; m++))
+        do
+                printf "."
+                sleep 1m
+        done
+        printf "\n"
+done
+
+
+
+
+
+
+
+
+
+
+:'
 source $HOME/.profile
 cd $HOME/massa/massa-client
 massa_wallet_address=$(./massa-client --pwd $massa_pass wallet_info | grep Address | awk '{ print $2 }')
 
 ./massa-client -p $massa_pass node_start_staking  $massa_wallet_address
 
-echo "tmux attach -t rolls"
 while true
 do
         balance=$(./massa-client --pwd $massa_pass wallet_info | grep "Balance" | awk '{ print $3 }' | sed 's/candidate=//;s/,//')
@@ -31,7 +69,7 @@ do
         done
         printf "\n"
 done
-
+'
 #          massa_logs=`journalctl -n 1 -u massa`
 #          if [[ $massa_logs == *"Send network event failed An error occurred during channel communication: Failed to send event"* ]]; then 
 #                  echo 'Restarting...'
