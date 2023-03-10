1ff98c6b739bd71a7aaea5df19caa0483b8c7be3
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-03-10 17:23:46 +0200

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/new.sh hello)
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/new.sh) hello
 
 echo $1
