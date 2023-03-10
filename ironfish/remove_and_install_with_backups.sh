883bccf4f5a1081982e19e7d755b6aa56212dde1
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-03-10 20:16:32 +0200

    Update remove_and_install_with_backups.sh

--- ironfish/remove_and_install_with_backups.sh
+++ ironfish/remove_and_install_with_backups.sh
@@ -3,9 +3,14 @@
 
 curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_docker.sh | bash
 
+cd $HOME
+docker-compose down -v
+docker-compose run --rm --entrypoint "./bin/run reset --confirm" ironfish
+rm -rf $HOME/.ironfish
+
+
 echo "alias ironfish='docker exec ironfish ./bin/run'" >> ~/.profile
 source ~/.profile
-
 
 sudo tee <<EOF >/dev/null $HOME/docker-compose.yaml
 version: "3.3"
@@ -25,42 +30,26 @@ services:
 EOF
 
 docker-compose pull && docker-compose up -d
-
 sleep 20
-
 IRONFISH_NODENAME=`docker exec ironfish ./bin/run config | grep nodeName | awk '{print $2}' | sed 's/\"//g' |  tr ',' ' '`
 echo 'export IRONFISH_NODENAME='$IRONFISH_NODENAME >> $HOME/.profile
 source ~/.profile
-
 echo $IRONFISH_NODENAME
-
 if [ -z $IRONFISH_NODENAME ]; then
         read -p "Введите ваше имя ноды (придумайте, без спецсимволов - только буквы и цифры): " IRONFISH_NODENAME
         echo 'export IRONFISH_NODENAME='$IRONFISH_NODENAME >> $HOME/.profile
         source ~/.profile
 fi
-
 echo $IRONFISH_NODENAME
-
 docker exec ironfish ./bin/run  wallet:create $IRONFISH_NODENAME
-
 docker exec ironfish ./bin/run  wallet:use $IRONFISH_NODENAME
-
 docker exec ironfish ./bin/run  config:set nodeName $IRONFISH_NODENAME
-
 docker exec ironfish ./bin/run  config:set blockGraffiti $IRONFISH_NODENAME
-
 docker exec ironfish ./bin/run  config:set minerBatchSize 60000
-
 docker exec ironfish ./bin/run  config:set enableTelemetry true
-
 sleep 5
-
 docker exec ironfish ./bin/run  status
-
 docker exec ironfish ./bin/run  wallet:export $IRONFISH_NODENAME
-
 docker exec ironfish ./bin/run  wallet:export $IRONFISH_NODENAME --mnemonic --language=English
-
 echo "docker-compose logs -f --tail=100"
 
