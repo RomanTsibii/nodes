2b36c5c345b4f78f8b9ec5c6e92bfa057b789372
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-03-16 18:55:39 +0200

    Update reinstall.sh

--- obol/reinstall.sh
+++ obol/reinstall.sh
@@ -20,16 +20,21 @@ rm -rf $HOME/charon-distributed-validator-node
 git clone https://github.com/ObolNetwork/charon-distributed-validator-node.git
 cd $HOME/charon-distributed-validator-node/
 
+git clone https://github.com/ObolNetwork/charon-distributed-validator-node.git
+cd $HOME/charon-distributed-validator-node/
 git checkout -- $HOME/charon-distributed-validator-node/docker-compose.yml
 cp $HOME/charon-distributed-validator-node/.env.sample $HOME/charon-distributed-validator-node/.env
 echo -e "\nGETH_PORT_HTTP=18545" >> $HOME/charon-distributed-validator-node/.env
 echo -e "\nLIGHTHOUSE_PORT_P2P=19000" >> $HOME/charon-distributed-validator-node/.env
 echo -e "\nMONITORING_PORT_GRAFANA=4000" >> $HOME/charon-distributed-validator-node/.env
-echo -e "\nCHARON_P2P_EXTERNAL_HOSTNAME=$(curl -s [ifconfig.me](http://ifconfig.me/))" >> $HOME/charon-distributed-validator-node/.env
+echo -e "\nCHARON_P2P_EXTERNAL_HOSTNAME=$(curl -s ifconfig.me)" >> $HOME/charon-distributed-validator-node/.env
 sed -i -e 's/9100:9100/19100:9100/' $HOME/charon-distributed-validator-node/docker-compose.yml
-mkdir $HOME/charon-distributed-validator-node/.charon/
-cp -r $HOME/backup_obol/.charon $HOME/charon-distributed-validator-node/chmod o+rw -R $HOME/charon-distributed-validator-node
+
+mkdir -p $HOME/charon-distributed-validator-node/.charon/
+cp -r $HOME/backup_obol/.charon $HOME/charon-distributed-validator-node/
+chmod o+rw -R $HOME/charon-distributed-validator-node
 sudo chown -R 1000:1000 $HOME/charon-distributed-validator-node/.charon/
+sudo chmod -R 777 $HOME/charon-distributed-validator-node/teku/
 
 tree $HOME/charon-distributed-validator-node/.charon
 
