303d59818a6a455d9fcdd37d247b48eb90c3ec6f
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-03-25 11:41:19 +0200

    Update starknet_remove_db.sh

--- starknet/starknet_remove_db.sh
+++ starknet/starknet_remove_db.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/starknet/starknet_remove_db.sh)
 
+du -sh $HOME/pathfinder/pathfinder
+
 cd $HOME/pathfinder/
 docker-compose down -v
 rm -rf $HOME/pathfinder/pathfinder
 mkdir -p $HOME/pathfinder/pathfinder
 chown -R 1000.1000 .
 docker-compose up -d
+
+du -sh $HOME/pathfinder/pathfinder
