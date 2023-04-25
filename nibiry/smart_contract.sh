e3478cacdab406e1195c0aa9ab6e005bb209ce2e
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-04-26 00:36:52 +0300

    Update smart_contract.sh

--- nibiry/smart_contract.sh
+++ nibiry/smart_contract.sh
@@ -49,7 +49,13 @@ nibid tx wasm inst $code_id "$(cat inst.json)" --label="mint CWXX contract" --no
 read -p "CONTRACT: " CONTRACT
 #CONTRACT=nibi1zhyznzp6ssn804nzj6a4gzmmam40vgwd5vnyyhtglaw8wwec8yjqeyxvcv
 
-TRANSFER='{"transfer":{"recipient":"nibi12lcsfjdymnta7yejww25em4f2j4cawh0nu42dd","amount":"50"}}'
+TRANSFER1='{"transfer":{"recipient":"nibi12lcsfjdymnta7yejww25em4f2j4cawh0nu42dd","amount":"50"}}'
+TRANSFER2='{"transfer":{"recipient":"nibi130axd6lmeyym5r6galsepqx8lnkgugpgkytjp0","amount":"50"}}'
+TRANSFER3='{"transfer":{"recipient":"nibi190fxan9uhtsg39vcjq8m3fdjka8eujdnq4zqlr","amount":"50"}}'
+TRANSFER4='{"transfer":{"recipient":"nibi1uzreuedcfq6tg7l48azvzxu8xnxfh3f2kmgk7g","amount":"50"}}'
+TRANSFER5='{"transfer":{"recipient":"nibi1g3scwmqrr4hnjpe6l68n5fh5x5vmltc0qasfql","amount":"50"}}'
+ARRAY=($TRANSFER1 $TRANSFER2 $TRANSFER3 $TRANSFER4 $TRANSFER5)
+TRANSFER=${ARRAY[RANDOM % 5]}
 
 nibid tx wasm execute $CONTRACT $TRANSFER --gas=3000000 --fees=80000unibi --from wallet -y
 
