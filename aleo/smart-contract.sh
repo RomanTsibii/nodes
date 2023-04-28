9374c5dd8c2dbe2f6932b4f18846174c430ac0a9
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-04-29 01:03:24 +0300

    Create smart-contract.sh

---
+++ aleo/smart-contract.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/aleo/smart-contract.sh)
+
+cd $HOME && cd leo_deploy && leo new $NAME
+
+echo Enter your Private Key: && read PK && \
+echo Enter your View Key: && read VK && \
+echo Enter your Address: && read ADDRESS
+
+echo Enter the Name of your contract "(any)": && read NAME
+
+echo Paste the link: && read QUOTE_LINK && \
+CIPHERTEXT=$(curl -s $QUOTE_LINK | jq -r '.execution.transitions[0].outputs[0].value')
+
+RECORD=$(snarkos developer decrypt --ciphertext $CIPHERTEXT --view-key $VK)
+
+snarkos developer deploy "$NAME.aleo" \
+--private-key "$PK" \
+--query "https://vm.aleo.org/api" \
+--path "$HOME/leo_deploy/$NAME/build/" \
+--broadcast "https://vm.aleo.org/api/testnet3/transaction/broadcast" \
+--fee 6000000 \
+--record "$RECORD"
