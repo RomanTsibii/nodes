4a1ccbec1b27cda2cc994c14f96cd634be35afb0
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-04-29 15:18:48 +0300

    Update and rename smart-contract-with-variables1.sh to smart-contract-with-variables2.sh

--- aleo/smart-contract-with-variables2.sh
+++ aleo/smart-contract-with-variables2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/aleo/smart-contract-with-variables1.sh) PK VK ADDRESS NAME QUOTE_LINK
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/aleo/smart-contract-with-variables2.sh) PK VK ADDRESS NAME QUOTE_LINK
 
 PK=$1 # PRIVATE_KEY
 VK=$2 # View Key
