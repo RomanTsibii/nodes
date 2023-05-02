0fa9b55cf379d2c62ad76f0e4564eafacfe1cd02
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-05-02 10:46:16 +0300

    Update smart_sleep.sh

--- gear/smart_sleep.sh
+++ gear/smart_sleep.sh
@@ -1,7 +1,15 @@
-
+# 1 - спосіб
 while true 
 do
   LOGS=`journalctl -n 5 -u gear`
   if [[ $LOGS == *"Syncing"* || $LOGS == *"якийсь інший вивід з логів"* ]]; then break ; fi
   sleep_seconds 3
+done
+
+# 2 - спосіб
+for I in 1 2 3 4 5 6 7 8 9 10
+do
+  LOGS=`journalctl -n 5 -u gear`
+  if [[ $LOGS == *"Syncing"* || $LOGS == *"якийсь інший вивід з логів"* ]]; then break ; fi 
+  sleep_seconds 4
 done
