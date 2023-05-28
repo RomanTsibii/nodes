668563d122de0a235c3684396db7a8711c3dbfb1
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-05-28 15:58:55 +0300

    Create stake.sh

---
+++ shardeum/stake.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/shardeum/stake.sh)
+
+# PRIV_KEY=$(dialog --inputbox "Enter your private key from your testnet Shardeum node(Metamask private key) :" 0 0 "" --stdout) && clear
+# WALLET_ADDR=$(dialog --inputbox "Enter your wallet address from your testnet Shardeum node(Metamask wallet) :" 0 0 "" --stdout) && clear
+
+PRIV_KEY=$1
+WALLET_ADDR=$2
+
+docker exec -it shardeum-dashboard operator-cli start
+sleep 30
+# docker exec -it -e PRIV_KEY=$PRIV_KEY shardeum-dashboard operator-cli stake 10
+docker exec -d shardeum-dashboard sh -c "(sleep 10; echo '${PRIV_KEY}'; sleep 10) | operator-cli stake 10"
+docker exec -it -e WALLET_ADDR=$WALLET_ADDR shardeum-dashboard operator-cli stake_info $WALLET_ADDR
