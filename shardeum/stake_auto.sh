668f8d5858e21ba03db3327c15f37d0ded65f077
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-05-29 16:28:12 +0300

    Create stake_auto.sh

---
+++ shardeum/stake_auto.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/shardeum/stake_auto.sh)
+
+if [ -z $STAKE_SHARDEUM ]; then
+        read -p "Set full comand for stake " STAKE_SHARDEUM
+        echo 'export STAKE_SHARDEUM='$STAKE_SHARDEUM >> $HOME/.profile
+        source ~/.profile
+fi
+
+$STAKE_SHARDEUM
