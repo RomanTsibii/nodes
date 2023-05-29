8016b5a2dc0b30469fe55494b6d236139e9b63f6
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-05-29 17:03:07 +0300

    Update and rename stake_auto.sh to add_stake_varibles.sh

--- shardeum/add_stake_varibles.sh
+++ shardeum/add_stake_varibles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/shardeum/stake_auto.sh)
+# bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/shardeum/add_stake_varibles.sh)
 if ! grep -q STAKE_SHARDEUM $HOME/.profile; then
         read -p "Set full comand for stake " STAKE_SHARDEUM
         echo 'alias STAKE_SHARDEUM='\'$STAKE_SHARDEUM\' >> $HOME/.profile
