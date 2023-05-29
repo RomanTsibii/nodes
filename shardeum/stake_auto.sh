03287f2a5e1a57d4fa6abbe2335dcdc786ee8014
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-05-29 16:45:03 +0300

    Update stake_auto.sh

--- shardeum/stake_auto.sh
+++ shardeum/stake_auto.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/shardeum/stake_auto.sh)
 
-if [ -z $STAKE_SHARDEUM ]; then
+if [ -z STAKE_SHARDEUM ]; then
         read -p "Set full comand for stake " STAKE_SHARDEUM
-        echo 'alias STAKE_SHARDEUM=''$STAKE_SHARDEUM' >> $HOME/.profile
+        echo 'alias STAKE_SHARDEUM=''STAKE_SHARDEUM' >> $HOME/.profile
         source ~/.profile
 fi
 
