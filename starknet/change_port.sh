e53c3406ed0a5ac63917fc1e687c19eb4f247e65
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-08-02 14:55:34 +0300

    Create change_port.sh

---
+++ starknet/change_port.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/starknet/change_port.sh)
+
+# Зупиняємо докер старкнету
+docker-compose -f $HOME/pathfinder/docker-compose.yml down
+
+# Указываем путь до файла config.json
+config_file="$HOME/pathfinder/docker-compose.yml"
+
+# Заменяем URL с помощью команды sed
+sed -i 's|- 9545:9545|9547:9547|' "$config_file"
+
+# Запускаємо докер старкнету
+docker-compose -f $HOME/pathfinder/docker-compose.yml up -d
+
+echo "docker-compose -f $HOME/pathfinder/docker-compose.yml logs -f --tail=50"
