59b9e03bab3cd424558d84f189434f8e1275b4fd
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-10-05 10:03:31 +0300

    Create restart.sh

---
+++ squid/restart.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/squid/restart.sh) 1 2 3 4 
+# start with params (1 or 2 or 3 or 4)
+
+echo $1
+FOLDER_SQUID=$1
+
+if $1=1
+then
+  FOLDER_SQUID="my-single-proc-squid"
+fi
+
+if $1=2
+then
+  FOLDER_SQUID="my-double-proc-squid"
+fi
+
+if $1=3
+then
+  FOLDER_SQUID="my-triple-proc-squid"
+fi
+
+if $1=4
+then
+  FOLDER_SQUID="my-quad-proc-squid"
+fi
+
+cd $FOLDER_SQUID
+
+sqd down
+
+docker-compose down
+
+sqd up
+
+npm ci
+
+sqd build
+
+sqd migration:apply
+
+sqd run .
