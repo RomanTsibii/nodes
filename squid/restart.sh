01e579dcbacda0ec815f42ecb909023c5b528b58
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-10-23 21:33:25 +0300

    Update restart.sh

--- squid/restart.sh
+++ squid/restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/squid/restart.sh) 1 2 3 4 
+# bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/squid/restart.sh) 1 2 3 4 5
 # start with params (1 or 2 or 3 or 4)
 
 echo $1
@@ -8,6 +8,7 @@ cd $HOME/my-single-proc-squid && docker-compose down
 cd $HOME/my-double-proc-squid && docker-compose down
 cd $HOME/my-triple-proc-squid && docker-compose down
 cd $HOME/my-quad-proc-squid && docker-compose down
+cd $HOME/my-snapshot-squid && docker-compose down
 
 if [ "$1" -eq "1" ]
 then
@@ -27,6 +28,11 @@ fi
 if [ "$1" -eq "4" ]
 then
   FOLDER_SQUID="my-quad-proc-squid"
+fi
+
+if [ "$1" -eq "5" ]
+then
+  FOLDER_SQUID="my-snapshot-squid"
 fi
 
 cd $HOME/$FOLDER_SQUID
