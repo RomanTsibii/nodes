1c426b44762a3e7616b95413f649ea889a9adda7
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-11-15 09:11:47 +0200

    Update update_new_wallet.sh

--- penumbra/update_new_wallet.sh
+++ penumbra/update_new_wallet.sh
@@ -2,7 +2,11 @@
 # bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/penumbra/update_new_wallet.sh)
 
 bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/penumbra/update_penumbra.sh)
-
+pcli --version
 rm -rf /root/.local/share/pcli/custody.json /root/.local/share/pcli/pcli-view.sqlite-shm
-pcli init soft-kms generate
-pcli view address
+
+pcli init soft-kms generate > penymbra.txt
+
+pcli view address >> penymbra.txt
+cat penymbra.txt
+tmux new-session -d -s penymbra_sync 'pcli view balance'
