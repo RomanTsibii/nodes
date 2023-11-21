d2d6701f864413e19e1afa03987643103e3ada73
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-11-21 15:03:00 +0200

    Update staking.sh

--- shardeum/staking.sh
+++ shardeum/staking.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 # bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/shardeum/staking.sh)
 
-docker exec -it shardeum-dashboard operator-cli start
-sleep 30
-
 tmux kill-session -t shardeum_healthcheck
 tmux new-session -d -s shardeum_healthcheck 'bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/shardeum/health.sh)'
-sleep 10
+docker exec -it shardeum-dashboard operator-cli start
+sleep 30
 source $HOME/.profile
 for ((n=0;n<10;n++)); do  STAKE_SHARDEUM;  sleep 10; done
