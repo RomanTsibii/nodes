4f55a8d60c09c8e1c831abde48eb5a4c240f248d
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-12-14 18:33:56 +0200

    Update restart.sh

--- squid/restart.sh
+++ squid/restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/squid/restart.sh) 1 2 3 4 5
+# bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/squid/restart.sh) 1 2 3 4 5 6 7
 # start with params (1 or 2 or 3 or 4)
 
 echo $1
@@ -33,6 +33,16 @@ fi
 if [ "$1" -eq "5" ]
 then
   FOLDER_SQUID="my-snapshot-squid"
+fi
+
+if [ "$1" -eq "6" ]
+then
+  FOLDER_SQUID="my-cryptopunks-squid"
+fi
+
+if [ "$1" -eq "7" ]
+then
+  FOLDER_SQUID="my-ens-squid"
 fi
 
 cd $HOME/$FOLDER_SQUID
