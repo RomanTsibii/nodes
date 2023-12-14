53670eea1d7e8dcd8761bea3e9afd80a87211b28
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-12-14 20:28:32 +0200

    Update crontab.sh

--- squid/crontab.sh
+++ squid/crontab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/starknet/remove_db_crontab.sh)
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/squid/crontab.sh)
 
 function sleep_min () {
   for((sec=0; sec<"$1"; sec++))
@@ -12,8 +12,23 @@ function sleep_min () {
 
 for i in {1..50}
 do
-  tmux new-session -d -s squid_restart 'bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/squid/restart.sh) 5'
-  sleep_min 15 
+  tmux new-session -d -s squid_restart 'bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/squid/restart.sh) 6'
+  sleep_min 60 
   tmux kill-session -t squid_restart
-  sleep 30
+  
+  tmux new-session -d -s squid_restart 'bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/squid/restart.sh) 6'
+  sleep_min 60
+  tmux kill-session -t squid_restart
+  
+  tmux new-session -d -s squid_restart 'bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/squid/restart.sh) 7'
+  sleep_min 360
+  tmux kill-session -t squid_restart
+  
+  tmux new-session -d -s squid_restart 'bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/squid/restart.sh) 7'
+  sleep_min 360
+  tmux kill-session -t squid_restart
+  
+  tmux new-session -d -s squid_restart 'bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/squid/restart.sh) 7'
+  sleep_min 120
+  tmux kill-session -t squid_restart
 done
