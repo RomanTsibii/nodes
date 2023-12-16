e8860ad3571131d0933bb646bd35ceea920f9f95
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2023-12-16 14:40:00 +0200

    Update update.sh

--- penumbra/update.sh
+++ penumbra/update.sh
@@ -3,7 +3,7 @@
 
 bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/penumbra/update_penumbra.sh)
 pcli view address
-pcli tx delegate 95penumbra --to penumbravalid1ufwmfmtsgfp952z4cnyg9yh9a53642kvq7828vkjjcskdartnqpqzr9zx6
-pcli tx delegate 95penumbra --to penumbravalid19jp2gwykhmeekn64snzfl6kvq9dwnqel2vdjcqmtpl5wgfdn6c8q47kn66
-pcli tx delegate 95penumbra --to penumbravalid1emegmxe5yr3xuq4fldmj4sjd55wus4hcsey9tc3mzqycuccc65gsqzyq0z
+pcli tx delegate 35penumbra --to penumbravalid1ufwmfmtsgfp952z4cnyg9yh9a53642kvq7828vkjjcskdartnqpqzr9zx6
+pcli tx delegate 35penumbra --to penumbravalid19jp2gwykhmeekn64snzfl6kvq9dwnqel2vdjcqmtpl5wgfdn6c8q47kn66
+pcli tx delegate 5penumbra --to penumbravalid1emegmxe5yr3xuq4fldmj4sjd55wus4hcsey9tc3mzqycuccc65gsqzyq0z
 pcli view staked
