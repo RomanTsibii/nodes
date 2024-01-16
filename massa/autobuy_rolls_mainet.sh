571fe009e8f63fd1463ae78928ed141b065449b3
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-01-16 19:57:09 +0200

    Update autobuy_rolls_mainet.sh

--- massa/autobuy_rolls_mainet.sh
+++ massa/autobuy_rolls_mainet.sh
@@ -2,7 +2,7 @@
 
 # pkill -9 tmux 
 # запустити автопокупку
-# tmux new-session -d -s massa_buy_rolls_mainnet 'https://raw.githubusercontent.com/RomanTsibii/nodes/main/massa/autobuy_rolls_mainet.sh massa_password'
+# tmux new-session -d -s massa_buy_rolls_mainnet 'bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/massa/autobuy_rolls_mainet.sh) massa_password'
 
 # зупинити автопокупку
 # tmux kill-session -t massa_buy_rolls_mainnet 
@@ -12,6 +12,7 @@
 # Ctrl+B і потім D - це щоб вийти з логів
 
 massa_pass=$1
+source $HOME/.profile
 
 cd $HOME/massa/massa-client
 massa_wallet_address=$(./massa-client -p $massa_pass wallet_info | grep Address | awk '{ print $2 }')
@@ -30,7 +31,7 @@ do
   if [ $int_balance -gt "99" ] ; then   
     resp=$(./massa-client -p $massa_pass buy_rolls $massa_wallet_address 1 0)
     echo "buy rolls"
-    if $BOT_TOKEN && $CHAT_ID; then   
+    if [[ ! -z $BOT_TOKEN && $CHAT_ID ]]; then   
         send_message "Buy 100 rolls. Server IP *$HOSTNAME*"
     fi
   else
