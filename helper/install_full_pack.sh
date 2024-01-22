7f76afceea5026cf30b118263f824b8fb87f9ffc
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-01-22 10:30:54 +0200

    Create install_full_pack.sh

---
+++ helper/install_full_pack.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/helper/install_full_pack.sh)
+
+# babylon
+
+
+# masa
+
+
+# penumbra
+
+
+# shardeum
+
+
+# elixir
+
+
+# holograph
+
