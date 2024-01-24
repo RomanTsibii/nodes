3b64ebe624ba46d73b4e90f83d3a527e5139505c
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-01-24 17:33:23 +0200

    Update install_full_pack.sh

--- helper/install_full_pack.sh
+++ helper/install_full_pack.sh
@@ -42,13 +42,16 @@ function babylon_install {
   wait_for_instaling
   source $HOME/.profile
   # keys_babylon=$(babylond keys add wallet)
-  send_message "Node Babylon was installed. On server *$HOSTNAME* to *$NODE_OWNER*."
+  send_message "Node #Babylon was installed. On server *$HOSTNAME* to *$NODE_OWNER*."
   # send_message "You keys *$keys_babylon*"
 }
 
 # masa
 function masa_install {
-  
+  bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/masa/install.sh)
+  sleep 10
+  MASA_PRIVATE=$(cat $HOME/.masa/masa_oracle_key.ecdsa)
+  send_message "Node #Masa was installed. On server *$HOSTNAME* to *$NODE_OWNER*.%0AYour node private *$MASA_PRIVATE*"
 }
 
 # penumbra
@@ -61,14 +64,17 @@ function penumbra_install {
       pcli init soft-kms generate >> penymbra.txt
       pcli view address >> penymbra.txt
       pemumbra_keys=$(cat penymbra.txt)
-      send_message "Node Penumbra was installed. On server *$HOSTNAME* to *$NODE_OWNER*.%0AYou keys *$pemumbra_keys*"
+      send_message "Node #Penumbra was installed. On server *$HOSTNAME* to *$NODE_OWNER*.%0AYou keys *$pemumbra_keys*"
   else
-      send_message "Node Penumbra was installed. On server *$HOSTNAME* to *$NODE_OWNER*.%0AImport old memo"
+      send_message "Node #Penumbra was installed. On server *$HOSTNAME* to *$NODE_OWNER*.%0AImport old memo"
   fi
 }
 
 # shardeum
 function shardeum_install {
+  # add stake variables
+  STAKE_SHARDEUM="bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/shardeum/stake.sh) $PRIVATE_ADDRESS $MM_ADDRESS"
+  echo 'alias STAKE_SHARDEUM='\'$STAKE_SHARDEUM\' >> $HOME/.profile 
   screen -S install -dm bash -c ". <(wget -qO- sh.doubletop.io) shardeum install"
   sleep 30
   screen -S install -X stuff $'\n' # press enter: INSTALL?
@@ -95,18 +101,68 @@ function shardeum_install {
   sleep 2400
   screen -S install -X stuff $'\n' # press enter: node installed
   # wait_for_instaling
-  
-  
-
-  
 }
 
 # elixir
 function elixir_install {
   
+  screen -S install -dm bash -c "bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/elixir/install.sh)"
+  sleep 15 
+  screen -S install -X stuff "$NODENAME" # validator name
+  sleep 1
+  screen -S install -X stuff $'\n' # press enter
+  sleep 5
+  screen -S install -X stuff "$MM_ADDRESS" # MM_ADDRESS
+  sleep 1
+  screen -S install -X stuff $'\n' # press enter
+  sleep 5
+  screen -S install -X stuff "$PRIVATE_ADDRESS" # PRIVATE_ADDRESS
+  sleep 1
+  screen -S install -X stuff $'\n' # press enter
+  sleep 5
+  wait_for_instaling
+  send_message "Node Elixir was installed. On server *$HOSTNAME* to *$NODE_OWNER*.%0A You can start stake and get role Alchemist(T2)"
 }
 
 # holograph
 function holograph_install {
-  
+  bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh)
+  bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/node.sh)
+  npm install -g @holographxyz/cli
+  sleep 30
+  screen -S install -dm bash -c "holograph config"
+  sleep 15
+  screen -S install -X stuff $' \e[B \e[B \n' # press space( ), arrow down(\e[B), space, arrow down, space, enter(\n)
+  sleep 2
+  screen -S install -X stuff $'\n' # goerli - enter
+  sleep 2
+  screen -S install -X stuff "https://polygon-mumbai-pokt.nodies.app" # polygon RPS
+  sleep 2
+  screen -S install -X stuff $'\n' # polygon - enter
+  sleep 2
+  screen -S install -X stuff "https://ava-testnet.public.blastapi.io/ext/bc/C/rpc" # avalanch RPS
+  sleep 2
+  screen -S install -X stuff $'\n' # polygon - enter
+  sleep 2
+  screen -S install -X stuff $'\n' # press enter
+  sleep 2
+  screen -S install -X stuff "$PRIVATE_ADDRESS" # NODE_PASSWORD
+  sleep 2
+  screen -S install -X stuff $'\n' # press enter
+  sleep 3
+  screen -S install -X stuff "$NODE_PASSWORD" # NODE_PASSWORD
+  sleep 2
+  screen -S install -X stuff $'\n' # press enter
 }
+
+function bevm_install {
+
+}
+
+bevm_install
+masa_install
+babylon_install
+penumbra_install
+elixir_install
+holograph_install
+shardeum_install
