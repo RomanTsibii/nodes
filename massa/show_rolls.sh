6c81afee551cbf5dfc4564e15cfa1af359d781cb
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-01-30 10:45:22 +0200

    Update show_rolls.sh

--- massa/show_rolls.sh
+++ massa/show_rolls.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
-# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/massa/show_rolls.sh)
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/massa/show_rolls.sh) massa_pass tg_chat_id tg_token
+# screen -S massa_balance_evelyday_TG_BOT -dm bash -c "bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/massa/show_rolls.sh) massa_pass tg_chat_id tg_token"
+# 
+
+massa_pass=$1
+CHAT_ID=$2
+BOT_TOKEN=$3
+HOSTNAME=$(hostname -I | awk '{ print $1 }')
+
+function send_message {
+    curl -s -X POST https://api.telegram.org/bot$BOT_TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="$1" -d parse_mode="markdown" > /dev/null
+}
 
 source $HOME/.profile
 cd $HOME/massa/massa-client
-balance=$(./massa-client --pwd $massa_pass wallet_info | grep "Balance")
-echo $balance
-candidate_rolls=$(./massa-client --pwd $massa_pass wallet_info | grep "Rolls")
-echo $candidate_rolls
+
+while true
+do
+  balance=$(./massa-client -p $massa_pass wallet_info | grep "Balance")
+  echo $balance
+  candidate_rolls=$(./massa-client -p $massa_pass wallet_info | grep "Rolls")
+  echo $candidate_rolls
+  send_message "*$HOSTNAME*%0A*$balance*%0A*$candidate_rolls*"
+
+  sleep 24h
+done
