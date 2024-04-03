54eac63a4c715b2d4858c44c6485a50487fc56ee
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-04-04 01:23:23 +0300

    Create avail_helscheck.sh

---
+++ helper/avail_helscheck.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/helper/avail_helscheck.sh) 
+
+while true
+  do
+    fail=$(tail -n 10 screenlog.0 | grep "Avail has been added to your profile.")
+    
+    if [ -n "$fail" ]
+    then
+          echo "\$var is NOT empty"
+          screen -S avail -X quit
+          screen -dmS avail -L
+          screen -S avail -X colon "logfile flush 0^M"  
+          screen -S avail -X stuff "curl -sL1 avail.sh | bash"
+          screen -S avail -X stuff $'\n' # press enter
+    fi
+    sleep 50
+  done
+  
