83a1088b6594e2aeb2ca6142534081c64a1da0df
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-04-05 00:41:50 +0300

    Update avail_helscheck.sh

--- helper/avail_helscheck.sh
+++ helper/avail_helscheck.sh
@@ -4,17 +4,16 @@
 
 while true
   do
-    fail=$(tail -n 10 screenlog.0 | grep "Avail has been added to your profile.")
-    fail1=(tail -n 10 screenlog.0 | grep "Avail stopped")
-    if [ -n "$fail"  ] || [ -n $fail1 ]
-    then
-          echo "\$var is NOT empty"
-          screen -S avail -X quit
-          screen -dmS avail -L
-          screen -S avail -X colon "logfile flush 0^M"  
-          screen -S avail -X stuff "curl -sL1 avail.sh | bash"
-          screen -S avail -X stuff $'\n' # press enter
-    fi
-    sleep 50
+    # fail=$(tail -n 10 screenlog.0 | grep "Avail has been added to your profile.")
+    # fail1=(tail -n 10 screenlog.0 | grep "Avail stopped")
+    # if [ -n "$fail"  ] || [ -n $fail1 ]
+    # then
+    screen -S avail -X quit
+    screen -dmS avail -L
+    screen -S avail -X colon "logfile flush 0^M"  
+    screen -S avail -X stuff "curl -sL1 avail.sh | bash"
+    screen -S avail -X stuff $'\n' # press enter
+    # fi
+    sleep 20m
   done
   
