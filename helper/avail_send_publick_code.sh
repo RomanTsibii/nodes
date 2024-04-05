ac6909056afa55f7b617c40c395fa2b3accc265c
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-04-05 13:20:43 +0300

    Update and rename avail_install.sh to avail_send_publick_code.sh

---
+++ helper/avail_send_publick_code.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/helper/avail_send_publick_code.sh) 
+
+source $HOME/.profile
+HOSTNAME=$(hostname -I | awk '{ print $1 }')
+function send_message {
+    curl -s -X POST https://api.telegram.org/bot$BOT_TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="$1" -d parse_mode="markdown" > /dev/null
+}
+
+PUBLIC=$(tail -n 1000 screenlog.0 | grep "public key:"  | awk '{print $11}' | head -1)
+send_message "Node #Avail installed. On server *$HOSTNAME* to.%0AYou public key *$PUBLIC*"
