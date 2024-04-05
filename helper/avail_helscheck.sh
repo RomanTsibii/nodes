06faef10e252c6201c0fa71690a692c59df51677
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-04-05 10:15:04 +0300

    Update avail_helscheck.sh

--- helper/avail_helscheck.sh
+++ helper/avail_helscheck.sh
@@ -8,6 +8,8 @@ while true
     # fail1=(tail -n 10 screenlog.0 | grep "Avail stopped")
     # if [ -n "$fail"  ] || [ -n $fail1 ]
     # then
+    screen -XS avail quit
+    sleep 1
     echo "restart avail" 
     screen -dmS avail -L
     sleep 1
@@ -18,6 +20,6 @@ while true
     screen -S avail -X stuff $'\n' # press enter
     # fi
     sleep 20m
-    screen -XS avail quit
+    
   done
   
