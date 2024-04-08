efe679a99e12677f499f8304ae2c5cc6f284e6ef
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-04-08 08:03:00 +0300

    Update avail_send_publick_code.sh

--- helper/avail_send_publick_code.sh
+++ helper/avail_send_publick_code.sh
@@ -7,5 +7,5 @@ function send_message {
     curl -s -X POST https://api.telegram.org/bot$BOT_TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="$1" -d parse_mode="markdown" > /dev/null
 }
 
-PUBLIC=$(tail -n 1000 screenlog.0 | grep "public key:"  | awk '{print $11}' | head -1)
+PUBLIC=$(tail -n 1000000 screenlog.0 | grep "public key:"  | awk '{print $11}' | head -1)
 send_message "Node #Avail installed. On server *$HOSTNAME* to.%0AYou public key *$PUBLIC*"
