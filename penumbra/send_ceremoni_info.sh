ab547a84737457296a8ef24b12a2ba828ee805f5
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-04-12 12:33:47 +0300

    Create send_ceremoni_info.sh

---
+++ penumbra/send_ceremoni_info.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/penumbra/send_ceremoni_info.sh)
+
+source $HOME/.profile
+HOSTNAME=$(hostname -I | awk '{ print $1 }')
+function send_message {
+    curl -s -X POST https://api.telegram.org/bot$BOT_TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="$1" -d parse_mode="markdown" > /dev/null
+}
+
+
+cer_info=$()
+echo $cer_info | grep "for contribution slot from address" | awk '{print $14}'
+send_message "Node #Penumbra ceremoni 2. On server *$HOSTNAME* to *$NODE_OWNER*.%0AYour ceremoni address %0A\`$cer_info\`"
+
