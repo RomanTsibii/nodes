17a5a29c3d2afaf40581b3ce6bcdea12a9500f61
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-04-12 15:34:12 +0300

    Create heals_check_send_stake.sh

---
+++ shardeum/heals_check_send_stake.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/shardeum/heals_check_send_stake.sh) 
+
+
+tmux kill-session -t shardeum_healthcheck
+tmux new-session -d -s shardeum_healthcheck 'bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/shardeum/health.sh)'
+sleep 200 
+source $HOME/.profile
+HOSTNAME=$(hostname -I | awk '{ print $1 }')
+function send_message {
+    curl -s -X POST https://api.telegram.org/bot$BOT_TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="$1" -d parse_mode="markdown" > /dev/null
+}
+
+status=$(docker exec -t shardeum-dashboard operator-cli status | grep state | awk '{ print $2 }')
+function send {
+    send_message "Node #Shardeum updated. You need stake *$HOSTNAME*:8080 .%0AYou public key \`$PRIVATE_ADDRESS\`"
+}
+if [[ $status == *"need-stake"* ]]; then send ; fi
+
