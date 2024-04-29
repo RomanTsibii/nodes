996752f59ea860f8aad23cfad962c11155e36479
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-04-29 23:35:42 +0300

    Update heals_check_send_stake.sh

--- shardeum/heals_check_send_stake.sh
+++ shardeum/heals_check_send_stake.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/shardeum/heals_check_send_stake.sh) 
 
+cd $HOME/.shardeum && ./docker-up.sh
+cd $HOME
 
 tmux kill-session -t shardeum_healthcheck
 tmux new-session -d -s shardeum_healthcheck 'bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/shardeum/health.sh)'
@@ -13,9 +15,18 @@ function send_message {
 
 status=$(docker exec -t shardeum-dashboard operator-cli status | grep state | awk '{ print $2 }')
 echo $status
-function send {
+function need_stake_send {
     send_message "Node #Shardeum updated. You need stake *$HOSTNAME*:8080 .%0AYou public key \`$PRIVATE_ADDRESS\`%0AAddress: \`$MM_ADDRESS\`"
     echo "send"
 }
-if [[ $status == *"need-stake"* ]]; then send ; fi
+if [[ $status == *"need-stake"* ]]; then need_stake_send ; fi
 
+function need_up {
+    send_message "Node #Shardeum updated. You need stake \`$HOSTNAME\`"
+    echo "send"
+}
+
+until [[ $status == *"need-stake"* ]] ||  [[ $status == *"active"* ]] || [[ $status == *"waiting-for-network"* ]] || [[ $status == *"standby"* ]] || [[ $status == *"stopped"* ]] || 
+do
+     need_up
+done
