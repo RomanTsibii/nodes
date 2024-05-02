3a5da675fbd31cdab423f1954d7b28107fa7dce2
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-05-02 15:15:20 +0300

    Update autobuy_rolls_mainet.sh

--- massa/autobuy_rolls_mainet.sh
+++ massa/autobuy_rolls_mainet.sh
@@ -26,7 +26,7 @@ function send_message {
 # запуск покупки ролів
 while true
 do
-  balance=$(./massa-client -p $massa_pass wallet_info | grep "Balance" | awk '{ print $3 }' | sed 's/candidate=//;s/,//')
+  balance=$(./massa-client -p $massa_pass wallet_info | grep $massa_wallet_address -A 3 | grep "Balance" | awk '{ print $3 }' | sed 's/candidate=//;s/,//')
   int_balance=${balance%%.*}
   if [ $int_balance -gt "99" ] ; then   
     resp=$(./massa-client -p $massa_pass buy_rolls $massa_wallet_address 1 0)
