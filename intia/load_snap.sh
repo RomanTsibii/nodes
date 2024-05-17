b37e62c1feeb95447d1794cc432f440fcd992f70
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-05-18 00:46:46 +0300

    Create load_snap.sh

---
+++ intia/load_snap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/intia/load_snap.sh)
+# screen -S load_snap_intia -dm bash -c "bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/intia/load_snap.sh)"
+
+sudo systemctl stop initia
+cp $HOME.initia/data/priv_validator_state.json $HOME.initia/priv_validator_state.json
+rm -rf $HOME.initia/data/
+curl -L https://snapshots.kjnodes.com/initia-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.initia
+systemctl restart initia
