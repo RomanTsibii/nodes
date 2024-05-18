daaeb1fba84b28fe1cd212e7e424c8465c9fce14
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-05-18 22:59:57 +0300

    Update install.sh

--- intia/install.sh
+++ intia/install.sh
@@ -3,15 +3,12 @@
 # bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/intia/install.sh)
 # screen -S intia_run -dm bash -c "bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/intia/install.sh)"
 
+#!/bin/bash
+
 function colors {
   GREEN="\e[32m"
   RED="\e[39m"
-  YELLOW="\e[33m"
   NORMAL="\e[0m"
-}
-
-function logo {
-  curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
 }
 
 function line {
@@ -22,184 +19,120 @@ function output {
   echo -e "${YELLOW}$1${NORMAL}"
 }
 
-function output_error {
-  echo -e "${RED}$1${NORMAL}"
-}
-
-function output_normal {
-  echo -e "${GREEN}$1${NORMAL}"
+function install_tools {
+  sudo apt update
+  sudo apt install -y curl git jq lz4 build-essential
 }
 
 function get_nodename {
     source $HOME/.profile
     INITIA_NODENAME=$NODENAME
-    if [ ! ${INITIA_NODENAME} ]; then
-        line
+    if [ -z "${INITIA_NODENAME}" ]; then
+        echo "Enter your nodename:"
         read INITIA_NODENAME
     fi
     echo 'export INITIA_NODENAME='$INITIA_NODENAME >> $HOME/.profile
-    echo "Your nodename $INITIA_NODENAME"
-    output "Your nodename $INITIA_NODENAME"
-}
-
-function source_git {
-    git clone https://github.com/initia-labs/initia $HOME/initia
-    cd $HOME/initia
-    git checkout v0.2.14
-    make install
 }
 
 function install_go {
-    sudo rm -rvf /usr/local/go/
-    wget https://golang.org/dl/go1.21.1.linux-amd64.tar.gz
-    sudo tar -C /usr/local -xzf go1.21.1.linux-amd64.tar.gz
-    rm go1.21.1.linux-amd64.tar.gz
-
-    echo "export GOROOT=/usr/local/go" >> $HOME/.profile 
-    echo "export GOPATH=$HOME/go" >> $HOME/.profile 
-    echo "export GO111MODULE=on" >> $HOME/.profile 
-    echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.profile 
-    source $HOME/.profile
+  sudo rm -rf /usr/local/go
+  curl -L https://go.dev/dl/go1.21.6.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+  echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile
+  source .bash_profile
 }
 
-function install_cosmovisor {
-    go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@v1.0.0
+function source_git {
+  cd && rm -rf initia
+  git clone https://github.com/initia-labs/initia
+  cd initia
+  git checkout v0.2.14
+  make install
 }
 
-function install_main_tools {
-    bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh) &>/dev/null
+function cli_configuration {
+  initiad config set client chain-id initiation-1
+  initiad config set client keyring-backend test
+  initiad config set client node tcp://localhost:25757
 }
 
-function init_node {
-    initiad init $INITIA_NODENAME --chain-id initiation-1
+function initialize {
+  initiad init "${INITIA_NODENAME}" --chain-id initiation-1
 }
 
-function config_node {
-    pruning="custom"
-    pruning_keep_recent="100"
-    pruning_keep_every="0"
-    pruning_interval="19"
-    EXTERNAL_IP=$(wget -qO- eth0.me)
-    PROXY_APP_PORT=14658
-    P2P_PORT=14656
-    PPROF_PORT=14060
-    API_PORT=14317
-    GRPC_PORT=14090
-    RPC_PORT=14657
-    GRPC_WEB_PORT=14091
-    PEERS="6c50666c45e8a86e04af84b0dcef29469ce284be@213.199.40.241:53456,1677252f64d728aa9598cb7365f74af7c862d9df@65.109.57.221:25756,5f934bd7a9d60919ee67968d72405573b7b14ed0@65.21.202.124:29656,04f0d493cb02a43d85b4fcd4bafd171500a433a0@162.55.27.107:46656,31da678c571c34cef11612d1ef166b9ea32829f4@149.50.96.124:39656"
-    SEEDS="ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@testnet-seeds.polkachu.com:25756"
-    curl -Ls https://snapshots.kjnodes.com/initia-testnet/genesis.json > $HOME/.initia/config/genesis.json
-    sed -i 's/seeds = ""/seeds = "ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@testnet-seeds.polkachu.com:25756"/' $HOME/.initia/config/config.toml
-    curl -Ls https://snapshots.kjnodes.com/initia-testnet/addrbook.json > $HOME/.initia/config/addrbook.json
-    sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.initia/config/app.toml
-    sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.initia/config/app.toml
-    sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.initia/config/app.toml
-    sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.initia/config/app.toml
-    sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.initia/config/config.toml
-
-    sed -i \
-        -e "s/\(proxy_app = \"tcp:\/\/\)\([^:]*\):\([0-9]*\).*/\1\2:$PROXY_APP_PORT\"/" \
-        -e "s/\(laddr = \"tcp:\/\/\)\([^:]*\):\([0-9]*\).*/\1\2:$RPC_PORT\"/" \
-        -e "s/\(pprof_laddr = \"\)\([^:]*\):\([0-9]*\).*/\1localhost:$PPROF_PORT\"/" \
-        -e "/\[p2p\]/,/^\[/{s/\(laddr = \"tcp:\/\/\)\([^:]*\):\([0-9]*\).*/\1\2:$P2P_PORT\"/}" \
-        -e "/\[p2p\]/,/^\[/{s/\(external_address = \"\)\([^:]*\):\([0-9]*\).*/\1${EXTERNAL_IP}:$P2P_PORT\"/; t; s/\(external_address = \"\).*/\1${EXTERNAL_IP}:$P2P_PORT\"/}" \
-        $HOME/.initia/config/config.toml
-    sed -i \
-        -e "/\[api\]/,/^\[/{s/\(address = \"tcp:\/\/\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$API_PORT\4/}" \
-        -e "/\[grpc\]/,/^\[/{s/\(address = \"\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$GRPC_PORT\4/}" \
-        -e "/\[grpc-web\]/,/^\[/{s/\(address = \"\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$GRPC_WEB_PORT\4/}" $HOME/.initia/config/app.toml
-
-    sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.initia/config/config.toml
-    sed -i -e "s|^node *=.*|node = \"tcp://localhost:14657\"|" $HOME/.initia/config/client.toml
-    sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.15uinit\"|" $HOME/.initia/config/app.toml
-
+function genesis_address {
+  curl -L https://snapshots-testnet.nodejumper.io/initia-testnet/genesis.json > $HOME/.initia/config/genesis.json
+  curl -L https://snapshots-testnet.nodejumper.io/initia-testnet/addrbook.json > $HOME/.initia/config/addrbook.json
 }
 
-function prepare_node {
-    mkdir -p $HOME/.initia/cosmovisor/genesis/bin
-    mkdir -p $HOME/.initia/cosmovisor/upgrades
-    cp $HOME/go/bin/initiad $HOME/.initia/cosmovisor/genesis/bin
+function set_seeds {
+  sed -i -e 's|^seeds *=.*|seeds = "2eaa272622d1ba6796100ab39f58c75d458b9dbc@34.142.181.82:26656,c28827cb96c14c905b127b92065a3fb4cd77d7f6@testnet-seeds.whispernode.com:25756,cd69bcb00a6ecc1ba2b4a3465de4d4dd3e0a3db1@initia-testnet-seed.itrocket.net:51656,093e1b89a498b6a8760ad2188fbda30a05e4f300@35.240.207.217:26656,2c729d33d22d8cdae6658bed97b3097241ca586c@195.14.6.129:26019"|' $HOME/.initia/config/config.toml
 }
 
-function snap_node {
-    curl -L https://snapshots.kjnodes.com/initia-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.initia
-### Thanks kjnodes for high-quality snapshots ###
+function gas_price {
+  sed -i -e 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.15uinit,0.01uusdc"|' $HOME/.initia/config/app.toml
 }
 
-function prepare_systemd {
-    sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
-Storage=persistent
-EOF
-    sudo systemctl restart systemd-journald
+function puring {
+  sed -i \
+    -e 's|^pruning *=.*|pruning = "custom"|' \
+    -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
+    -e 's|^pruning-interval *=.*|pruning-interval = "17"|' \
+    $HOME/.initia/config/app.toml
+}
 
-    sudo tee /etc/systemd/system/initia.service > /dev/null <<EOF
+function ports {
+  sed -i -e "s%:1317%:25717%; s%:8080%:25780%; s%:9090%:25790%; s%:9091%:25791%; s%:8545%:25745%; s%:8546%:25746%; s%:6065%:25765%" $HOME/.initia/config/app.toml
+  sed -i -e "s%:26658%:25758%; s%:26657%:25757%; s%:6060%:25760%; s%:26656%:25756%; s%:26660%:25761%" $HOME/.initia/config/config.toml
+}
+
+function last_snap {
+  curl -L https://snapshots.polkachu.com/testnet-snapshots/initia/initia_187918.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.initia
+}
+
+function create_service {
+  sudo tee /etc/systemd/system/initiad.service > /dev/null << EOF
 [Unit]
-Description="initia node"
+Description=Initia node service
 After=network-online.target
-
 [Service]
 User=$USER
-ExecStart=$HOME/go/bin/cosmovisor start
-Restart=always
-RestartSec=3
-LimitNOFILE=4096
-Environment="DAEMON_NAME=initiad"
-Environment="DAEMON_HOME=$HOME/.initia"
-Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"
-Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
-Environment="UNSAFE_SKIP_BACKUP=true"
-
+ExecStart=$(which initiad) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
 [Install]
 WantedBy=multi-user.target
 EOF
-
-    sudo systemctl daemon-reload
-    sudo systemctl enable initia
-    sudo systemctl restart initia
+  sudo systemctl daemon-reload
+  sudo systemctl enable initiad.service
 }
 
-function check {
-    bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/initia/check.sh)
-}
-
-function load_snap_screen {
-  screen -S load_snap_intia -dm bash -c "bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/intia/load_snap.sh)"
-}
 function main {
     colors
     line
-    logo
-    line
     output "Welcome to the Initia installation script"
     line
-    output "Your nodename $INITIA_NODENAME"
+    install_tools
+    line
     get_nodename
     line
     output "Installing Initia..."
     line
-    install_main_tools
     install_go
-    install_cosmovisor
     source_git
-    init_node
-    config_node
-    prepare_node
-    # snap_node
-    prepare_systemd
+    cli_configuration
+    initialize
+    genesis_address
+    set_seeds
+    gas_price
+    puring
+    ports
+    last_snap
+    create_service
     line
     output_normal "Installation complete"
     line
-    output "Cheking node Initia"
-    line
-    check
-    line
-    output "Wish lifechange case with DOUBLETOP"
-    #line
-    #output "Stop and load snapshot in screen"
-    #line
-    load_snap_screen
-    #line
+    sudo systemctl start initiad.service
 }
 
 main
