b6468f8f511a72911c487414ea1f2f5946fdec64
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-05-27 20:40:01 +0300

    Update update.sh

--- penumbra/update.sh
+++ penumbra/update.sh
@@ -3,23 +3,4 @@
 
 bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/penumbra/update_penumbra.sh)
 
-array[0]="5penumbra"
-array[1]="10penumbra"
-array[2]="15penumbra"
-array[3]="20penumbra"
-array[4]="25penumbra"
-array[5]="30penumbra"
-array[6]="35penumbra"
-array[7]="40penumbra"
-
-random_loop=$((1 + RANDOM % 3))
-size1=${#array[@]}
-for i in $(seq $random_loop); do  
-  index=$(($RANDOM % $size1))
-  echo ${array[$index]}
-
-  # pcli tx delegate ${array[$index]} --to ${validaaddress[$index]}
- pcli tx delegate ${array[$index]} --to $(pcli query validator list | grep penumbravalid | awk '{print $6}' | shuf -n 1)
-done
-sleep 5
-pcli view staked
+screen -S synk_penumbra -dm bash -c "pcli view staked"
