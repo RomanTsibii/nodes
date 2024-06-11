3ee5d3cf48dc1722ecc2e879eac197b49a05aea7
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-06-11 13:35:45 +0300

    Update ceremoni_2.sh

--- penumbra/ceremoni_2.sh
+++ penumbra/ceremoni_2.sh
@@ -1,22 +1,50 @@
 #!/bin/bash
 # bash <(curl -s  https://raw.githubusercontent.com/RomanTsibii/nodes/main/penumbra/stake.sh)
-# screen -S penumbra_ceremoni_2 -dm bash -c "bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/penumbra/ceremoni_2.sh) tokens"
+# screen -S run_penumbra_ceremoni_2 -dm bash -c "bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/penumbra/ceremoni_2.sh) tokens"
+
+# вписати кількість токенів з скрита
+tokens=$1
+
+# Перевірка, чи існує змінна $tokens
+if [ -n "$tokens" ]; then
+  ceremoni_balance=$tokens
+else
+  # Отримати баланс
+  balance=$(pcli view balance | grep "penumbra " | awk '{print $3}' | sed 's/penumbra//')
+  balance_number=$(echo $balance | awk '{print $1 + 0}') # Конвертуємо у числове значення
+
+  # Згенерувати випадкове число від 5 до 50
+  random_number=$(shuf -i 5-50 -n 1)
+
+  # Відняти випадкове число від балансу
+  new_balance=$((balance_number - random_number))
+  ceremoni_balance=$new_balance
+fi
+
+# Вивести результати
+echo "Ceremoni balance: $ceremoni_balance"
 
 
+# добавляємо до числового значення  {550penumbra}
+ceremoni_balance_with_suffix="${ceremoni_balance}penumbra"
 
-balance=$(pcli view balance | grep "penumbra " | awk '{print $3}' | sed 's/penumbra//')
-balance_number=$(echo $balance | awk '{print $1 + 0}') # Конвертуємо у числове значення
 
-# Згенерувати випадкове число від 5 до 50
-random_number=$(shuf -i 5-50 -n 1)
+SESSION_NAME="penumbra_ceremoni_2"
+LOG_FILE="/var/log/penumbra_ceremoni_2.log"
 
-# Відняти випадкове число від балансу
-new_balance=$((balance_number - random_number))
+# Перевірка, чи існує сесія
+if screen -list | grep -q "$SESSION_NAME"; then
+  echo "Сесія $SESSION_NAME вже існує. Закриття..."
+  screen -S "$SESSION_NAME" -X quit
+fi
 
-# добавляємо до числового значення 
-new_balance_with_suffix="${new_balance}penumbra"
+# Запуск нової сесії
+screen -dmS "$SESSION_NAME" -Logfile "$LOG_FILE"
+sleep 1
 
-echo $new_balance_with_suffix
-# запустити церемонію 
-pcli ceremony contribute --phase 2 --bid $new_balance_with_suffix
+# Виконання команди в новій сесії
+screen -S "$SESSION_NAME" -X stuff "pcli ceremony contribute --phase 2 --bid $ceremoni_balance_with_suffix"
+sleep 1
+screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
 
+# запустити цикл на очікування завершення сесії і надіслати з сервера хеш церемонії на телеграм 
