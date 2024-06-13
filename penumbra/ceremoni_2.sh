1fbc1e6a88b20f8e289a4c7debbecbd7ec847f74
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-06-13 16:22:03 +0300

    Update ceremoni_2.sh

--- penumbra/ceremoni_2.sh
+++ penumbra/ceremoni_2.sh
@@ -4,58 +4,54 @@
 # глянути логи  - "tail -f -n 10 /var/log/penumbra_ceremoni_2.log"
 # глянути останній рялок логу - "tail -n 1 /var/log/penumbra_ceremoni_2.log"
 
-# вписати кількість токенів з скрита
-# tokens=$1
-# Перевірка, чи існує змінна $tokens
-# if [ -n "$tokens" ]; then
-#   ceremoni_balance=$tokens
-# else
-  # Отримати баланс
+# Отримати баланс
+function full_balance {
   balance=$(pcli view balance | grep "penumbra " | awk '{print $3}' | sed 's/penumbra//')
   balance_number=$(echo $balance | awk '{print $1 + 0}') # Конвертуємо у числове значення
   rounded_balance=$(printf "%.0f" "$balance_number") # Заокруглюємо до цілого значення
+  echo $rounded_balance
+}
 
+function balance_for_ceremoni {
   # Перевірка на мінімальний баланс
   if [ "$rounded_balance" -gt 99 ]; then
     # Згенерувати випадкове число від 5 до 10
     random_number=$(shuf -i 5-10 -n 1)
-
+  
     # Відняти випадкове число від балансу 
     new_balance=$((rounded_balance - random_number))
     ceremoni_balance=$new_balance
-  # Перевірка на максимальний баланс
-  else #
-    ceremoni_balance=0
   fi
-# fi
+  # Вивести результати
+  echo "Ceremoni balance: $ceremoni_balance"
+  # добавляємо до числового значення  {550penumbra}
+  ceremoni_balance_with_suffix="${ceremoni_balance}penumbra"
+}
 
-# Вивести результати
-echo "Ceremoni balance: $ceremoni_balance"
+function remove_old_session {
+  SESSION_NAME="penumbra_ceremoni_2"
+  LOG_FILE="/var/log/penumbra_ceremoni_2.log"
+  # Перевірка, чи існує сесія
+  if screen -list | grep -q "$SESSION_NAME"; then
+    echo "Сесія $SESSION_NAME вже існує. Закриття..."
+    screen -ls | grep "$SESSION_NAME" | awk '{print $1}' | xargs -I {} screen -S {} -X quit
+  fi
+}
 
+function create_new_session {
+  # Запуск нової сесії
+  screen -dmS "$SESSION_NAME"
+  sleep 1
+  # добавити файл логування 
+  screen -S "$SESSION_NAME" -X logfile "$LOG_FILE"
+  screen -S "$SESSION_NAME" -X log on
+  # Виконання команди в новій сесії
+  screen -S "$SESSION_NAME" -X stuff "pcli ceremony contribute --phase 2 --bid $ceremoni_balance_with_suffix"
+  sleep 1
+  screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
+}
 
-# добавляємо до числового значення  {550penumbra}
-ceremoni_balance_with_suffix="${ceremoni_balance}penumbra"
-
-
-SESSION_NAME="penumbra_ceremoni_2"
-LOG_FILE="/var/log/penumbra_ceremoni_2.log"
-
-# Перевірка, чи існує сесія
-if screen -list | grep -q "$SESSION_NAME"; then
-  echo "Сесія $SESSION_NAME вже існує. Закриття..."
-  screen -ls | grep "$SESSION_NAME" | awk '{print $1}' | xargs -I {} screen -S {} -X quit
-
-fi
-
-# Запуск нової сесії
-screen -dmS "$SESSION_NAME"
-sleep 1
-# добавити файл логування 
-screen -S "$SESSION_NAME" -X logfile "$LOG_FILE"
-screen -S "$SESSION_NAME" -X log on
-# Виконання команди в новій сесії
-screen -S "$SESSION_NAME" -X stuff "pcli ceremony contribute --phase 2 --bid $ceremoni_balance_with_suffix"
-sleep 1
-screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
-
-# запустити цикл на очікування завершення сесії і надіслати з сервера хеш церемонії на телеграм 
+full_balance
+balance_for_ceremoni
+remove_old_session
+create_new_session
