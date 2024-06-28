fc07171b545759153017d73b321891e4e10c52d2
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-06-28 12:14:11 +0300

    Update setup_new_server.sh

--- setup_new_server.sh
+++ setup_new_server.sh
@@ -4,8 +4,9 @@
 # оновити час
 echo "set Ukraine timezone"
 sudo timedatectl set-timezone Europe/Kiev
-# встановити докер
-bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh)
+
+# adds ssh keys
+bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/add_ssh_keys.sh)
 
 # встановити tmux
 # встановити ncdu
@@ -14,5 +15,5 @@ sudo apt install ncdu tmux htop screen -y
 # install proxy for adspower
 bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/helper/proxy_for_antick.sh)
 
-# adds ssh keys
-bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/add_ssh_keys.sh)
+# встановити докер
+bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh)
