bdbf9a1b71c071b35c27a9a1bd0ed64fd1dad9e1
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-07-04 12:16:00 +0300

    Create bulk_check_logs.sh

---
+++ nubit/bulk_check_logs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Виконати команду docker exec і зберегти результат
+output=$(docker exec -it nubit /home/nubit-user/nubit-node/bin/nubit version)
+
+# Перевірити наявність потрібного тексту в результаті
+if [[ "$output" == *"Semantic version: v0.1.0-rc.2-39-ga7b2879-dev"* ]]; then
+    echo "Текст знайдено! в контейнері nubit"
+else
+    echo "Текст не знайдено! в контейнері nubit"
+fi
+
+
+for i in {1..10}
+do
+    # Виконати команду docker exec для кожного контейнера
+    output=$(docker exec -it nubit-$i /home/nubit-user/nubit-node/bin/nubit version)
+
+    # Перевірити наявність потрібного тексту в результаті
+    if [[ "$output" == *"Semantic version: v0.1.0-rc.2-39-ga7b2879-dev"* ]]; then
+        echo "Текст знайдено в контейнері nubit-$i!"
+    else
+        echo "Текст не знайдено в контейнері nubit-$i."
+    fi
+done
