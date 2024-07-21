e234641d97dd63df454a2df97614f07b3515b203
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-07-21 17:38:09 +0300

    Update update_rps.sh

--- waku/update_rps.sh
+++ waku/update_rps.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/waku/update_rps.sh) rps_sepolia
 
 RPS=$1
-
 docker-compose -f /root/nwaku-compose/docker-compose.yml down
-
-root/nwaku-compose/.env
-ETH_CLIENT_ADDRESS=
-
+sed -i "s|^ETH_CLIENT_ADDRESS=.*|ETH_CLIENT_ADDRESS=${RPS}|" /root/nwaku-compose/.env
 docker-compose -f /root/nwaku-compose/docker-compose.yml up -d
