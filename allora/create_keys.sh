a603eb450b8857e5afc6c236cd43f67c63e10306
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-07-27 00:33:09 +0300

    Create create_keys.sh

---
+++ allora/create_keys.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/allora/create_keys.sh) pass
+# screen -S allora_allora -dm bash -c "bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/allora/create_keys.sh) pass" 
+
+pass=$1
+
+SESSION_NAME="allora_keys"
+LOG_FILE="/var/log/allora_keys.log"
+
+# Запуск нової сесії
+screen -dmS "$SESSION_NAME"
+sleep 1
+# добавити файл логування
+screen -S "$SESSION_NAME" -X logfile "$LOG_FILE"
+screen -S "$SESSION_NAME" -X log on
+# Виконання команди в новій сесії
+screen -S "$SESSION_NAME" -X stuff "source ~/.profile && allorad keys add testkey"
+sleep 1
+screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
+sleep 3
+screen -S "$SESSION_NAME" -X stuff "$pass"
+sleep 1
+screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
+sleep 1
+screen -S "$SESSION_NAME" -X stuff "$pass"
+sleep 1
+screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
+
+echo "cat /var/log/allora_keys.log"
