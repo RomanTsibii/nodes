e5d9b1b4f915f61c1aed5a08df82a89835bdb1cf
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-08-17 12:32:57 +0300

    Update create_new_wallets.sh

--- allora/create_new_wallets.sh
+++ allora/create_new_wallets.sh
@@ -7,23 +7,32 @@ pass=$1
 SESSION_NAME="allora_keys_new"
 LOG_FILE="/var/log/allora_keys_new.log"
 
-# Запуск нової сесії
 screen -dmS "$SESSION_NAME"
 sleep 1
-# добавити файл логування
 screen -S "$SESSION_NAME" -X logfile "$LOG_FILE"
 screen -S "$SESSION_NAME" -X log on
-# Виконання команди в новій сесії
-screen -S "$SESSION_NAME" -X stuff "source ~/.profile && allorad keys add testkey"
-sleep 1
-screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
-sleep 3
-screen -S "$SESSION_NAME" -X stuff "$pass"
-sleep 1
-screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
-sleep 1
-screen -S "$SESSION_NAME" -X stuff "Y"
-sleep 1
-screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
 
-echo "cat /var/log/allora_keys.log"
+for ((i=1; i<=10; i+=1)); do  # echo $i  ; done
+  # Виконання команди в новій сесії
+  screen -S "$SESSION_NAME" -X stuff "source ~/.profile && allorad keys add testkey"
+  sleep 1
+  screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
+  sleep 3
+  screen -S "$SESSION_NAME" -X stuff "$pass"
+  sleep 1
+  screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
+  sleep 1
+  screen -S "$SESSION_NAME" -X stuff "Y"
+  sleep 1
+  screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
+  sleep 2
+  # echo "cat $LOG_FILE"
+  
+  seed=$(cat $LOG_FILE | grep password -2 | tail -1)
+  address=$(cat $LOG_FILE | grep address | awk '{print $3}')
+  
+  echo "------------------"
+  echo $seed
+  echo $address
+  rm /var/log/allora_keys_new.log
+done
