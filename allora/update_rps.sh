bac7aa1db1dd2a4e297718981a7d50f990ea372a
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-08-19 11:09:39 +0300

    Create update_rps.sh

---
+++ allora/update_rps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/allora/update_rps.sh)
+
+echo "Введите RPC"
+read RPC
+
+docker compose -f $HOME/allora-huggingface-walkthrough/docker-compose.yaml down
+sed -i "/\"nodeRpc\"/ s|\"nodeRpc\": \".*\"|\"nodeRpc\": \"$RPC\"|" $HOME/allora-huggingface-walkthrough/config.json
+
+cd allora-huggingface-walkthrough
+chmod +x init.config
+./init.config
+docker compose -f $HOME/allora-huggingface-walkthrough/docker-compose.yaml up -d
