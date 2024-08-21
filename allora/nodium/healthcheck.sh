10fda6db1cb50dee7db5ecf5758a631ce1f6fddc
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-08-22 00:27:39 +0300

    Update healthcheck.sh

--- allora/nodium/healthcheck.sh
+++ allora/nodium/healthcheck.sh
@@ -2,8 +2,8 @@
 # bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/allora/nodium/healthcheck.sh)
 # screen -S allora_healthcheck -dm bash -c "bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/allora/nodium/healthcheck.sh)" 
 
-# Константа для перевірки часу в секундах (2 години)
-CHECK_INTERVAL=$((60 * 60 * 2))
+# Константа для перевірки часу в секундах (40 хвилин)
+CHECK_INTERVAL=$((60 * 40))
 
 # Масив з шляхами до docker-compose.yml файлів
 DOCKER_COMPOSE_PATHS=(
@@ -20,24 +20,38 @@ check_and_restart() {
     local last_log_time=$(docker compose -f "$compose_file" logs worker | grep "Send Worker Data to chain" | grep txHash | awk -F'"time":' '{split($2, a, ","); print a[1]}' | tail -1)
 
     if [ -z "$last_log_time" ]; then
-        echo "[$(date)] Помилка: Не вдалося знайти час у логах або логи порожні для $compose_file. Пропускаємо перевірку."
+        echo "[$(date)] Помилка: Не вдалося знайти час у логах або логи порожні для $compose_file. Перезапускаємо Docker-контейнери..."
+        docker compose -f "$compose_file" restart
     elif [ $((current_time - last_log_time)) -gt $CHECK_INTERVAL ]; then
-        echo "[$(date)] Не було даних протягом останніх 2 годин, перезапускаємо контейнер $compose_file..."
+        echo "[$(date)] Не було даних протягом останніх 40 хвилин, перезапускаємо контейнер $compose_file..."
         if ! docker compose -f "$compose_file" restart; then
             echo "[$(date)] Помилка: Не вдалося перезапустити контейнер $compose_file."
         fi
     else
-        echo "[$(date)] Дані надходили протягом останніх 2 годин, перезапуск не потрібен. $compose_file"
+        echo "[$(date)] Дані надходили протягом останніх 40 хвилин, перезапуск не потрібен. $compose_file"
     fi
 }
 
 # Вічний цикл
 while true; do
+    # Перевірка кількості запущених контейнерів
+    container_count=$(docker ps | grep allora | wc -l)
+    if [ "$container_count" -ne 9 ]; then
+        echo "[$(date)] Кількість запущених контейнерів allora не дорівнює 9, перезапускаємо необхідні контейнери..."
+        docker compose -f $HOME/worker1-10m/docker-compose.yml restart
+        docker compose -f $HOME/worker2-24h/docker-compose.yml restart
+        docker compose -f $HOME/worker3-20m/docker-compose.yml restart
+    else
+        echo "[$(date)] Кількість запущених контейнерів allora дорівнює 9, додатковий перезапуск не потрібен."
+    fi
+
+    # Перевірка логів для кожного контейнера
     for compose_file in "${DOCKER_COMPOSE_PATHS[@]}"; do
         check_and_restart "$compose_file"
     done
 
-    # Очікування 90 хвилин перед наступною перевіркою
-    sleep 90m
+    # Очікування 10 хвилин перед наступною перевіркою
+    sleep 10m
 done
+
 
