bcaa0c830c09b862a934b958a411023cc04a27e1
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-08-24 12:59:14 +0300

    Create update_RPC.sh

---
+++ allora/nodium/update_RPC.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/allora/nodium/update_RPC.sh)
+
+echo "Введите RPC"
+read RPC
+
+# зупинка контейнерів
+docker compose -f $HOME/worker1-10m/docker-compose.yml down -v
+docker compose -f $HOME/worker2-24h/docker-compose.yml down -v
+docker compose -f $HOME/worker3-20m/docker-compose.yml down -v 
+
+# видалити всі контейнери з алорою 
+docker ps -a | grep allora | awk '{print $1}' | xargs docker rm -f
+
+# видалити всі images з алорою 
+docker images | grep worker | awk '{print $3}' | xargs docker rmi -f
+
+# видалити всі network з алорою 
+docker network rm worker1-10m_default worker2-24h_default worker3-20m_default
+
+# видалити всі volume з алорою 
+docker volume prune -f
+
+
+# заміна рпс 
+cd worker1-10m
+sed -i "/\"nodeRpc\"/ s|\"nodeRpc\": \".*\"|\"nodeRpc\": \"$RPC\"|" config.json
+./init.config
+cd ..
+
+
+cd worker2-24h
+sed -i "/\"nodeRpc\"/ s|\"nodeRpc\": \".*\"|\"nodeRpc\": \"$RPC\"|" config.json
+./init.config
+cd ..
+
+cd worker3-20m
+sed -i "/\"nodeRpc\"/ s|\"nodeRpc\": \".*\"|\"nodeRpc\": \"$RPC\"|" config.json
+./init.config
+cd ..
+
+# запуск контейнерів
+docker compose -f $HOME/worker1-10m/docker-compose.yml up -d --build
+docker compose -f $HOME/worker2-24h/docker-compose.yml up -d --build
+docker compose -f $HOME/worker3-20m/docker-compose.yml up -d --build
