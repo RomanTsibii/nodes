09865db0f733159fd8a68caa8e3479067ac3596f
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-08-24 13:37:34 +0300

    Update update_RPC.sh

--- allora/nodium/update_RPC.sh
+++ allora/nodium/update_RPC.sh
@@ -10,16 +10,16 @@ docker compose -f $HOME/worker2-24h/docker-compose.yml down -v
 docker compose -f $HOME/worker3-20m/docker-compose.yml down -v 
 
 # видалити всі контейнери з алорою 
-docker ps -a | grep allora | awk '{print $1}' | xargs docker rm -f
+# docker ps -a | grep allora | awk '{print $1}' | xargs docker rm -f
 
-# видалити всі images з алорою 
-docker images | grep worker | awk '{print $3}' | xargs docker rmi -f
+# # видалити всі images з алорою 
+# docker images | grep worker | awk '{print $3}' | xargs docker rmi -f
 
-# видалити всі network з алорою 
-docker network rm worker1-10m_default worker2-24h_default worker3-20m_default
+# # видалити всі network з алорою 
+# docker network rm worker1-10m_default worker2-24h_default worker3-20m_default
 
-# видалити всі volume з алорою 
-docker volume prune -f
+# # видалити всі volume з алорою 
+# docker volume prune -f
 
 
 # заміна рпс 
