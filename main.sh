e8da734a35f051c6fa67218be3183aa7a2eec2db
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-08-28 10:12:33 +0300

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/main.sh)
+
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh | bash 
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/ufw.sh | bash 
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh | bash 
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/go.sh | bash 
