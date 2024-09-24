d049332376f860c2245a56a0ff3a4f5aa2e66131
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-09-25 01:57:01 +0300

    Update change_model.sh

--- allora/nodium/change_model.sh
+++ allora/nodium/change_model.sh
@@ -65,5 +65,5 @@ done
 rm -rf $HOME/allora_models/
 # запуск контейнерів
 docker compose -f $HOME/worker1-10m/docker-compose.yml up -d --build
-docker compose -f $HOME/worker2-24h/docker-compose.yml up -d --build
+# docker compose -f $HOME/worker2-24h/docker-compose.yml up -d --build
 docker compose -f $HOME/worker3-20m/docker-compose.yml up -d --build
