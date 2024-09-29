3ad8d6c96d88d8e27a0853f0ec5508785825c1ac
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-09-29 20:58:44 +0300

    Update restart.sh

--- rivalz/restart.sh
+++ rivalz/restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/rivalz/install.sh)
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/rivalz/restart.sh)
 # sudo journalctl -u rivalz.service  -n 80 -f
 
 loop_devices=$(losetup -a | grep "fake_disk.img" | awk -F: '{print $1}')
