4b956e36827f1c5db6e633eea711371b790fb7e6
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-10-01 01:53:08 +0300

    Update install.sh

--- gAIaNET/install.sh
+++ gAIaNET/install.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 # bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/gAIaNET/install.sh)
-cd
+cd /root
 sudo apt update -y 
-sudo apt-get update
+# sudo apt-get update
 curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/install.sh' | bash
 
 source ~/.bashrc
 gaianet init --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen2-0.5b-instruct/config.json
+
+gaianet run
+gaianet_info=$(gaianet info)
+Node_ID=$(echo "$gaianet_info" | awk -F 'Node ID: ' '{print $2}' | awk '{print $1}')
+Device_ID=$(echo "$gaianet_info" | awk -F 'Device ID: ' '{print $2}')
+
+echo "Node_ID: $Node_ID"
+echo "Device_ID: $Device_ID"
+
+gaianet stop
+
+cd /root/gaianet 
+wget -O phrases.txt https://raw.githubusercontent.com/RomanTsibii/nodes/refs/heads/main/gAIaNET/phrases.txt
+wget -O bot_config.json https://raw.githubusercontent.com/RomanTsibii/nodes/refs/heads/main/gAIaNET/bot_config.json
+wget -O bot_gaia.sh https://raw.githubusercontent.com/RomanTsibii/nodes/refs/heads/main/gAIaNET/bot_gaia.sh
+chmod +x bot_gaia.sh
+# sed -i "s/YOUR_WALLET/$Node_ID/" config.json
+jq --arg node_id "$Node_ID" '.url = "https://\($node_id).us.gaianet.network/v1/chat/completions"' bot_config.json > temp.json && mv temp.json bot_config.json
+sed -i 's/\\u001b\[0m//g' bot_config.json
 
 sudo bash -c "cat > /etc/systemd/system/gaianet.service" <<EOL
 [Unit]
@@ -14,9 +33,10 @@ Description=GaiaNet Node Service
 After=network.target
 
 [Service]
-Type=forking
+Type=oneshot
 ExecStart=/root/gaianet/bin/gaianet run
-ExecStop=/root/gaianet/bin/gaianet stop
+ExecStartPost=/bin/bash -c '/root/gaianet/bot_gaia.sh &'
+RemainAfterExit=true
 PIDFile=/var/run/gaianet.pid
 Restart=on-failure
 User=root
@@ -33,39 +53,3 @@ sudo systemctl enable gaianet
 sudo systemctl start gaianet
 sudo systemctl status gaianet --no-pager
 
-gaianet_info=$(gaianet info)
-Node_ID=$(echo "$gaianet_info" | awk -F 'Node ID: ' '{print $2}' | awk '{print $1}')
-Device_ID=$(echo "$gaianet_info" | awk -F 'Device ID: ' '{print $2}')
-
-echo "Node_ID: $Node_ID"
-echo "Device_ID: $Device_ID"
-
-sudo apt install python3-pip nano screen -y
-pip install requests faker
-wget -O random_chat_with_faker.py https://raw.githubusercontent.com/RomanTsibii/nodes/refs/heads/main/gAIaNET/random_chat_with_faker.py
-
-sed -i "s/АДРЕСВАШЕГОКОШЕЛЬКА/$Node_ID/g" $HOME/random_chat_with_faker.py
-sed -i 's/\x1B\[[0-9;]*[a-zA-Z]//g' $HOME/random_chat_with_faker.py
-
-if [ "$#" -ne 2 ]; then
-    echo "Used sleep fron 60 to 180"
-else
-    new_min="$1"
-    new_max="$2"
-    sed -i "s/random.randint(60, 180)/random.randint($new_min, $new_max)/g" $HOME/random_chat_with_faker.py
-    echo "Used sleep fron $new_min to $new_max"
-fi
-
-SESSION_NAME="faker_session_gAIa"
-
-if screen -list | grep -q "$SESSION_NAME"; then
-  echo "$SESSION_NAME exixts. close"
-  screen -ls | grep "$SESSION_NAME" | awk '{print $1}' | xargs -I {} screen -S {} -X quit
-fi
-
-screen -dmS "$SESSION_NAME"
-sleep 1
-# Виконання команди в новій сесії
-screen -S "$SESSION_NAME" -X stuff "python3 ~/random_chat_with_faker.py"
-sleep 1
-screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
