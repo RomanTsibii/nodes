23e38088a6c2870905c6a46503d6928ca67181d8
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-10-01 19:23:08 +0300

    Create stop.sh

---
+++ gAIaNET/stop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Зупиняємо всі процеси, пов'язані з gaianet
+gaianet stop
+
+ps aux | grep gaianet | grep -v grep | awk '{print $2}' | while read -r pid; do
+    kill "$pid"
+done
+
+# Зупиняємо всі процеси, пов'язані з bot_gaia.sh
+ps aux | grep bot_gaia.sh | grep -v grep | awk '{print $2}' | while read -r pid; do
+    kill "$pid"
+done
+echo "ps aux | grep gaianet"
+echo "ps aux | grep bot_gaia.sh"
