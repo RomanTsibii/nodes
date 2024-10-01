4cb02de2b477279de877257fcb791ba3906d2ab7
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-10-01 13:58:57 +0300

    Update install.sh

--- gAIaNET/install.sh
+++ gAIaNET/install.sh
@@ -27,6 +27,23 @@ chmod +x bot_gaia.sh
 jq --arg node_id "$Node_ID" '.url = "https://\($node_id).us.gaianet.network/v1/chat/completions"' bot_config.json > temp.json && mv temp.json bot_config.json
 sed -i 's/\\u001b\[0m//g' bot_config.json
 
+sudo bash -c "cat > /etc/systemd/system/bot_gaia.service" <<EOL
+[Unit]
+Description=Bot Gaia Service
+After=gaianet.service
+Requires=gaianet.service
+
+[Service]
+ExecStart=/bin/bash /root/gaianet/bot_gaia.sh
+Restart=on-failure
+User=root
+WorkingDirectory=/root/gaianet
+Environment=GAIANET_ENV=production
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
 sudo bash -c "cat > /etc/systemd/system/gaianet.service" <<EOL
 [Unit]
 Description=GaiaNet Node Service
@@ -35,7 +52,6 @@ After=network.target
 [Service]
 Type=oneshot
 ExecStart=/root/gaianet/bin/gaianet run
-ExecStartPost=/bin/bash -c '/root/gaianet/bot_gaia.sh &'
 RemainAfterExit=true
 PIDFile=/var/run/gaianet.pid
 Restart=on-failure
@@ -47,9 +63,12 @@ Environment=GAIANET_ENV=production
 WantedBy=multi-user.target
 EOL
 
-
 sudo systemctl daemon-reload
-sudo systemctl enable gaianet
-sudo systemctl start gaianet
-sudo systemctl status gaianet --no-pager
+sudo systemctl enable gaianet.service
+sudo systemctl enable bot_gaia.service
+sudo systemctl start gaianet.service
+sudo systemctl start bot_gaia.service
+# sudo systemctl status gaianet --no-pager
 
+echo "journalctl -xeu gaianet.service -f"
+echo "journalctl -xeu bot_gaia.service -f"
