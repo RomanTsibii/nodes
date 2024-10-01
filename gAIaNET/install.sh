350c95a12684162e3bfa0fb4b8f424cb67037327
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-10-01 20:27:16 +0300

    Update install.sh

--- gAIaNET/install.sh
+++ gAIaNET/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/gAIaNET/install.sh)
 # tail -f /var/log/bot_gaia.log
-# tail -f /var/log/gaianet.log
+# tail -f /var/log/bot_gaia.log
 # 
-# nohup /root/gaianet/bot_gaia.sh >> /var/log/bot_gaia.log 2>&1 &   # start
-#
+# nohup /root/gaianet/bot_gaia.sh >> /var/log/bot_gaia.log 2>&1 &       # start
+# kill $(ps aux | grep bot_gaia.sh | grep -v grep | awk '{print $2}')   # stop
 #
 
 cd /root
@@ -13,10 +13,11 @@ sudo apt update -y
 curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/install.sh' | bash
 
 source ~/.bashrc
-gaianet init --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen2-0.5b-instruct/config.json
+/root/gaianet/bin/gaianet init --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen2-0.5b-instruct/config.json
 
-gaianet run
-gaianet_info=$(gaianet info)
+/root/gaianet/bin/gaianet run
+sleep 10
+gaianet_info=$(/root/gaianet/bin/gaianet info)
 Node_ID=$(echo "$gaianet_info" | awk -F 'Node ID: ' '{print $2}' | awk '{print $1}')
 Device_ID=$(echo "$gaianet_info" | awk -F 'Device ID: ' '{print $2}')
 
