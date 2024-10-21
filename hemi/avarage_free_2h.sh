e97530694d5e42b710e627d6162b386931d3dc10
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-10-21 16:47:10 +0300

    Create avarage_free_2h.sh

---
+++ hemi/avarage_free_2h.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/hemi/avarage_free_2h.sh)
+
+# Отримання даних з API
+response=$(curl -s https://mempool.space/testnet/api/v1/statistics/2h)
+
+# Витягнення значень total_fee в масив
+total_fees=($(echo "$response" | jq '.[].total_fee'))
+
+# Обчислення суми
+sum=0
+count=0
+
+for fee in "${total_fees[@]}"; do
+    sum=$((sum + fee))
+    count=$((count + 1))
+done
+
+# Знаходження середнього значення
+if [ $count -ne 0 ]; then
+    average=$((sum / count))
+    average=$((average / 100000000))  # Ділимо на 100000000
+else
+    average=0
+fi
+
+# Заміна значення у файлі конфігурації
+sudo sed -i "s/Environment=\"POPM_STATIC_FEE=.*/Environment=\"POPM_STATIC_FEE=$average\"/" /etc/systemd/system/hemi.service
+
+# Перезапуск сервісу
+sudo systemctl daemon-reload
+sudo systemctl restart hemi.service
+
+# Виведення результатів
+
+echo "Average of total fees: $average"
