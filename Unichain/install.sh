3e41abbae34bd26a6634c670a5ff9c9b18fdc68c
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-10-25 16:29:08 +0300

    Create install.sh

---
+++ Unichain/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/Unichain/install.sh)
+# sudo journalctl -u rivalz.service  -n 80 -f
+
+
+git clone https://github.com/Uniswap/unichain-node
+cd /root/unichain-node
