8a8f2905c9f57d2717b61369014df22964504c49
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-10-25 16:29:27 +0300

    Update install.sh

--- Unichain/install.sh
+++ Unichain/install.sh
@@ -5,3 +5,6 @@
 
 git clone https://github.com/Uniswap/unichain-node
 cd /root/unichain-node
+sed -i '/^OP_NODE_L1_ETH_RPC/c\OP_NODE_L1_ETH_RPC=https://ethereum-sepolia-rpc.publicnode.com' .env.sepolia
+
+
