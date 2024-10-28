01da4a808ccc855670be4a44c6de8e30ded63c79
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-10-28 23:18:28 +0200

    Create crontab.sh

---
+++ hemi/crontab.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/hemi/crontab.sh)
+# видалити кронтаб
+crontab -l | grep -v "/root/scripts/hemi_min_free_2h.sh" | crontab -
+
+# Створюємо папку scripts, якщо її не існує
+mkdir -p /root/scripts
+
+# Скачуємо файл і перейменовуємо його
+curl -o /root/scripts/hemi_min_free_2h.sh https://raw.githubusercontent.com/RomanTsibii/nodes/main/hemi/min_free_2h.sh
+
+# Надаємо права на виконання файлу
+chmod +x /root/scripts/hemi_min_free_2h.sh
+
+# Додаємо завдання в crontab для запуску кожні 2 години на випадковій хвилині
+# Випадкове число від 0 до 59 для хвилин
+minute=$((RANDOM % 60))
+
+# Записуємо завдання в crontab
+(crontab -l 2>/dev/null; echo "$minute */2 * * * /root/scripts/hemi_min_free_2h.sh >> /root/scripts/hemi_min_free_2h.log 2>&1") | crontab -
