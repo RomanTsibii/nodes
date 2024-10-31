b0b38c5f18a6f82e28c8c236af6cabc757b52b83
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-10-31 19:51:15 +0200

    Update install.sh

--- Unichain/install.sh
+++ Unichain/install.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 # bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/Unichain/install.sh)
-# sudo journalctl -u rivalz.service  -n 80 -f
+# docker-compose logs -f --tail=100
+# docker-compose logs -f | grep "Syncing: state download in progress"
+# curl -d '{"id":1,"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest",false]}' -H "Content-Type: application/json" http://localhost:8547
+# 
+function colors {
+  GREEN="\e[32m"
+  YELLOW="\e[33m"
+  RED="\e[39m"
+  NORMAL="\e[0m"
+}
 
+
+function install_docker {
+    if ! type "docker" > /dev/null; then
+        echo -e "${YELLOW}Устанавливаем докер${NORMAL}"
+        bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh)
+    else
+        echo -e "${YELLOW}Докер уже установлен. Переходим на следующий шаг${NORMAL}"
+    fi
+}
+
+
+sudo apt update # && sudo apt upgrade -y
+
+install_docker
+docker --version
 
 git clone https://github.com/Uniswap/unichain-node
 cd /root/unichain-node
 sed -i '/^OP_NODE_L1_ETH_RPC/c\OP_NODE_L1_ETH_RPC=https://ethereum-sepolia-rpc.publicnode.com' .env.sepolia
+sed -i '/^OP_NODE_L1_BEACON/c\OP_NODE_L1_BEACON=https://ethereum-sepolia-beacon-api.publicnode.com' .env.sepolia
 
+поміняти в докер файлі порт на з 8545 на 8547 і з start_interval на interval
 
+docker-compose up -d
+echo "You Private Key"
+echo $(cat geth-data/geth/nodekey)
