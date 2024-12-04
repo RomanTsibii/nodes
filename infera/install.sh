c63d5a64b6e8873fab1df4ffec8811b3b2cbd5c7
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-12-04 19:29:33 +0200

    Create install.sh

---
+++ infera/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/nillion/install.sh)
+
+function colors {
+  GREEN="\e[32m"
+  YELLOW="\e[33m"
+  RED="\e[39m"
+  NORMAL="\e[0m"
+}
+
+
+function install_docker {
+    if ! type "docker" > /dev/null; then
+        echo -e "${YELLOW}Устанавливаем докер${NORMAL}"
+        bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh)
+    else
+        echo -e "${YELLOW}Докер уже установлен. Переходим на следующий шаг${NORMAL}"
+    fi
+}
+
+
+curl -fsSL https://ollama.com/install.sh | sh
+rm -rf ~/infera
+curl -O https://www.infera.org/scripts/infera-linux-intel.sh
+chmod +x ./infera-linux-intel.sh
+./infera-linux-intel.sh
+grep 'alias init-infera' ~/.bashrc
+source ~/.bashrc
+mkdir ~/infera-docker
+cd ~/infera-docker
+cp ~/infera ~/infera-docker/
