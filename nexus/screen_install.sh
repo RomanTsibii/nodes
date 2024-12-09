7c3e7e38860cad0fb9710f60372e770f4e649f7a
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-12-09 22:36:50 +0200

    Update screen_install.sh

--- nexus/screen_install.sh
+++ nexus/screen_install.sh
@@ -9,9 +9,13 @@ else
   read -p "Enter your prover id: " prover_id
 fi
 
-
 SESSION_NAME="nexus"
 LOG_FILE="/var/log/nexus.log"
+
+if screen -list | grep -q "$SESSION_NAME"; then
+  echo "Сесія $SESSION_NAME вже існує. Закриття..."
+  screen -ls | grep "$SESSION_NAME" | awk '{print $1}' | xargs -I {} screen -S {} -X quit
+fi
 
 # Запуск нової сесії
 screen -dmS "$SESSION_NAME"
