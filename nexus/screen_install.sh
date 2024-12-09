6e6296a21b6f29882fea5a2622d8c52396bb3f7b
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-12-09 22:34:30 +0200

    Update screen_install.sh

--- nexus/screen_install.sh
+++ nexus/screen_install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/nexus/screen_install.sh)
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/nexus/screen_install.sh) prover_id
+
+
+
+if [ -n "$1" ]; then
+  prover_id="$1"
+else
+  read -p "Enter your prover id: " prover_id
+fi
+
 
 SESSION_NAME="nexus"
 LOG_FILE="/var/log/nexus.log"
@@ -15,6 +24,9 @@ screen -S "$SESSION_NAME" -X stuff "bash <(curl -s https://raw.githubusercontent
 sleep 1
 screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
 sleep 10
+
+screen -S "$SESSION_NAME" -X stuff "$prover_id" # натискання Enter
+sleep 1
 screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
 
 # screen -wipe > /dev/null 2>&1
