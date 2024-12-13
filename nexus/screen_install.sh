2ada56a370fb32bac4641ab7672bc6699578dc04
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-12-14 01:40:17 +0200

    Update screen_install.sh

--- nexus/screen_install.sh
+++ nexus/screen_install.sh
@@ -17,25 +17,20 @@ if screen -list | grep -q "$SESSION_NAME"; then
   screen -ls | grep "$SESSION_NAME" | awk '{print $1}' | xargs -I {} screen -S {} -X quit
 fi
 
-mkdir -p /root/.nexus && cd /root/.nexus
-echo "$prover_id" > prover-id
-# Запуск нової сесії
-screen -dmS "$SESSION_NAME"
-sleep 1
-# добавити файл логування
-screen -S "$SESSION_NAME" -X logfile "$LOG_FILE"
-screen -S "$SESSION_NAME" -X log on
-# Виконання команди в новій сесії
-screen -S "$SESSION_NAME" -X stuff "bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/nexus/install.sh)"
-sleep 1
-screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
-sleep 10
+# mkdir -p /root/.nexus && cd /root/.nexus
+# echo "$prover_id" > prover-id
+# # Запуск нової сесії
+# screen -dmS "$SESSION_NAME"
+# sleep 1
+# # добавити файл логування
+# screen -S "$SESSION_NAME" -X logfile "$LOG_FILE"
+# screen -S "$SESSION_NAME" -X log on
+# # Виконання команди в новій сесії
+# screen -S "$SESSION_NAME" -X stuff "bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/nexus/install.sh)"
+# sleep 1
+# screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
+# sleep 10
 
-screen -S "$SESSION_NAME" -X stuff "$prover_id" # натискання Enter
-sleep 1
-screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
-
-# screen -wipe > /dev/null 2>&1
-# screen -list | grep -q "$SESSION_NAME" && screen -S "$SESSION_NAME" -X quit
-
-# tail -3 $LOG_FILE | grep Points | awk '{print $NF}'
+# screen -S "$SESSION_NAME" -X stuff "$prover_id" # натискання Enter
+# sleep 1
+# screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
