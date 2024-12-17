0f52c37953930f6c95d148b9d330c13222958527
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2024-12-17 18:56:51 +0200

    Update restart.sh

--- gAIaNET/restart.sh
+++ gAIaNET/restart.sh
@@ -10,7 +10,7 @@
 kill $(ps aux | grep bot_gaia.sh | grep -v grep | awk '{print $2}')   # stop
 /root/gaianet/bin/gaianet stop
 
-# rebild
+# rebuild
 /root/gaianet/bin/gaianet init --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen2-0.5b-instruct/config.json
 
 # start
