73b430dc81a86671d062de00dc7dd2c32c9d4ba1
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2025-01-25 17:07:22 +0200

    Create prover_install.sh

---
+++ cysic/prover_install.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/cysic/prover_install.sh) address
+# 
+# логи
+# supervisorctl tail -f cysic-prover
+
+
+if [ -n "$1" ]; then
+  address="$1"
+else
+  read -p "Enter your address: " address
+fi
+
+apt-get install htop -y
+apt update
+apt install -y supervisor
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.6.3/local_installers/cuda-repo-ubuntu2204-12-6-local_12.6.3-560.35.05-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-6-local_12.6.3-560.35.05-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-12-6-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-6
+curl -L https://github.com/cysic-labs/phase2_libs/releases/download/v1.0.0/setup_prover.sh >  ~/setup_prover.sh
+chmod +x setup_prover.sh
+
+bash ~/setup_prover.sh $address
+
+echo '[unix_http_server]
+file=/tmp/supervisor.sock   ; the path to the socket file
+
+[supervisord]
+logfile=/tmp/supervisord.log ; main log file; default $CWD/supervisord.log
+logfile_maxbytes=50MB        ; max main logfile bytes b4 rotation; default 50MB
+logfile_backups=10           ; # of main logfile backups; 0 means none, default 10
+loglevel=info                ; log level; default info; others: debug,warn,trace
+pidfile=/tmp/supervisord.pid ; supervisord pidfile; default supervisord.pid
+nodaemon=false               ; start in foreground if true; default false
+silent=false                 ; no logs to stdout if true; default false
+minfds=1024                  ; min. avail startup file descriptors; default 1024
+minprocs=200                 ; min. avail process descriptors;default 200
+strip_ansi=true
+
+[rpcinterface:supervisor]
+supervisor.rpcinterface_factory=supervisor.rpcinterface:make_main_rpcinterface
+
+[supervisorctl]
+serverurl=unix:///tmp/supervisor.sock ; use a unix:// URL  for a unix socket
+
+[program:cysic-prover]
+command=/root/cysic-prover/prover
+numprocs=1
+directory=/root/cysic-prover
+priority=999
+autostart=true
+redirect_stderr=true
+stdout_logfile=/root/cysic-prover/cysic-prover.log
+stdout_logfile_maxbytes=1GB
+stdout_logfile_backups=1
+environment=LD_LIBRARY_PATH="/root/cysic-prover",CHAIN_ID="534352"' > supervisord.conf
+supervisord -c supervisord.conf
+sleep 20
+supervisorctl tail cysic-prover
+echo "DONE"
+echo "Logs: supervisorctl tail -f cysic-prover"
