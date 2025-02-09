ab8ee5d9ca6c5b943330a1709004333b1da50761
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2025-02-09 19:40:21 +0200

    Create update.sh

---
+++ Spheron/update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/Spheron/install.sh) token
+# 
+# логи
+# docker-compose -f ~/.spheron/fizz/docker-compose.yml logs  -f
+# видалити кронтаб 
+sudo crontab -l | grep -v "@reboot sleep 120; /root/scripts/spheron/restart.sh" | sudo crontab -
+sudo crontab -l | grep -v "/bin/bash /root/scripts/spheron/restart.sh" | sudo crontab -
+
+mkdir -p scripts/spheron && cd scripts/spheron
+wget -O restart.sh https://raw.githubusercontent.com/RomanTsibii/nodes/refs/heads/main/Spheron/restart.sh
+chmod +x restart.sh
+
+./restart.sh
+
+(sudo crontab -l ; echo "@reboot sleep 120;/bin/bash /root/scripts/spheron/restart.sh") | sudo crontab -
+RANDOM_MIN=$((RANDOM % 60));(sudo crontab -l ; echo "$RANDOM_MIN */3 * * * /bin/bash /root/scripts/spheron/restart.sh") | sudo crontab -
