a98fbdd43cce36b3ed24581b2824f99a67fe42c7
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2025-02-19 02:59:14 +0200

    Create run_expect.sh

---
+++ nexus/run_expect.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/expect -f
+
+set timeout -1
+spawn cargo run --release -- --start --beta
+
+expect {
+    "Do you want to use the existing user account? (y/n)" {
+        send "n\r"
+        exp_continue
+    }
+    -re {Enter '2' to start earning NEX} {
+        send "2\r"
+        exp_continue
+    }
+    "Please enter your node ID:" {
+        set fp [open "/root/.nexus/prover-id" r]
+        set node_id [gets $fp]
+        close $fp
+        send "$node_id\r"
+    }
+}
+
+interact
