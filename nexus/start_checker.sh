c53beab5cc1fcd3ae03de0c00145cb993c0a01d1
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2025-02-21 13:17:41 +0200

    Update start_checker.sh

--- nexus/start_checker.sh
+++ nexus/start_checker.sh
@@ -3,14 +3,14 @@
 # видалити кронтаб
 crontab -l | grep -v '/root/nexus_checker.sh' | crontab -
 
-# 1. Завантаження скрипта у каталог /root
 curl -o /root/nexus_checker.sh https://raw.githubusercontent.com/RomanTsibii/nodes/refs/heads/main/nexus/nexus_checker.sh
 
-# 2. Дозвіл на виконання скрипта
 chmod +x /root/nexus_checker.sh
 
-# 3. Додавання cron-запису (кожні 10 хвилин)
+if ! crontab -l 2>/dev/null | grep -q '^SHELL=/bin/bash'; then
+    (crontab -l 2>/dev/null; echo "SHELL=/bin/bash") | crontab -
+fi
+
 (crontab -l 2>/dev/null; echo "*/10 * * * * /root/nexus_checker.sh >> /var/log/nexus_checker.log 2>&1") | crontab -
 
-# Запуск
 /root/nexus_checker.sh >> /var/log/nexus_checker.log
