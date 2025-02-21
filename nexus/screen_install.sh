db0a0b655746e4d9e51ffe3e769bfb86f37a21b8
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2025-02-21 11:58:40 +0200

    Update screen_install.sh

--- nexus/screen_install.sh
+++ nexus/screen_install.sh
@@ -25,7 +25,8 @@ screen -S "$SESSION_NAME" -X logfile "$LOG_FILE"
 screen -S "$SESSION_NAME" -X log on
 # Виконання команди в новій сесії
 # screen -S "$SESSION_NAME" -X stuff "bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/nexus/install.sh)"
-screen -S "$SESSION_NAME" -X stuff "bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/nexus/testnet2.sh) $prover_id"
+# screen -S "$SESSION_NAME" -X stuff "bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/nexus/testnet2.sh) $prover_id"
+screen -S "$SESSION_NAME" -X stuff "curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/nexus/testnet2.sh | bash -s $prover_id"
 sleep 1
 screen -S "$SESSION_NAME" -X stuff $'\n' # натискання Enter
 sleep 1
