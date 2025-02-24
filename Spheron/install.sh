fe8b5fa2cc0adbb5fdb141f1d7d061b38062e96a
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2025-02-24 15:09:10 +0200

    Update install.sh

--- Spheron/install.sh
+++ Spheron/install.sh
@@ -50,8 +50,8 @@ screen -S "$SCREEN_NAME" -X stuff "sphnctl fizz start --token $USER_TOKEN"
 sleep 1
 screen -S "$SCREEN_NAME" -X stuff $'\n' # press enter
 
-MAX_RETRIES=120 # 120 * 10 = 1200 секунд(20хв) - очікувати поки не запуститься контейнер сперону
-
+MAX_RETRIES=120 # 120 * 25 = 3000 секунд(50хв) - очікувати поки не запуститься контейнер сперону
+echo "Install spehron"
 while [ 0 -lt $MAX_RETRIES ]; do
     # Перевірка, чи існує контейнер "spheronnetwork"
     if docker ps | grep -q "spheronnetwork"; then
@@ -59,7 +59,7 @@ while [ 0 -lt $MAX_RETRIES ]; do
         break
     else
         echo "."
-        sleep 10
+        sleep 25
     fi
     ((counter++))
 done
@@ -71,7 +71,16 @@ mkdir -p scripts/spheron && cd scripts/spheron
 wget -O restart.sh https://raw.githubusercontent.com/RomanTsibii/nodes/refs/heads/main/Spheron/restart.sh
 chmod +x restart.sh
 
-./restart.sh
+sudo curl -o /etc/systemd/system/cheker_spheron.service https://raw.githubusercontent.com/RomanTsibii/nodes/refs/heads/main/Spheron/cheker_spheron.service
+# sudo chmod 644 /etc/systemd/system/cheker_spheron.service
+# sudo chown root:root /etc/systemd/system/cheker_spheron.service
 
-(sudo crontab -l ; echo "@reboot sleep 120;/bin/bash /root/scripts/spheron/restart.sh") | sudo crontab -
-RANDOM_MIN=$((RANDOM % 60));(sudo crontab -l ; echo "$RANDOM_MIN */3 * * * /bin/bash /root/scripts/spheron/restart.sh") | sudo crontab -
+sudo systemctl daemon-reload
+sudo systemctl enable cheker_spheron.service
+sudo systemctl start cheker_spheron.service
+
+docker-compose -f ~/.spheron/fizz/docker-compose.yml down
+docker-compose -f ~/.spheron/fizz/docker-compose.yml up -d
+
+# (sudo crontab -l ; echo "@reboot sleep 120;/bin/bash /root/scripts/spheron/restart.sh") | sudo crontab -
+# RANDOM_MIN=$((RANDOM % 60));(sudo crontab -l ; echo "$RANDOM_MIN */3 * * * /bin/bash /root/scripts/spheron/restart.sh") | sudo crontab -
