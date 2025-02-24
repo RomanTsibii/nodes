60ac9dbf3e01172003c0b1bab6074c5990fc5677
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2025-02-24 22:16:00 +0200

    Update free_memory.sh

--- helper/free_memory.sh
+++ helper/free_memory.sh
@@ -2,6 +2,9 @@
 
 # bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/helper/free_memory.sh)
 
+# видалити логи у докері якщо там більше 1гб
+sudo find /var/lib/docker/containers/ -type f -name "*.log" -size +1G -exec truncate -s 0 {} \;
+
 # remove from docker all images with tag "none"
 docker images -q -a | xargs docker inspect --format='{{.Id}}{{range $rt := .RepoTags}} {{$rt}} {{end}}'|grep -v ':'
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
@@ -14,10 +17,6 @@ rm -rf /var/log/syslog /var/log/syslog.1 /var/log/syslog.2
 
 sudo journalctl --vacuum-size=100M
 sudo systemctl restart systemd-journald
-
-# видалити логи у докері якщо там більше 1гб
-sudo find /var/lib/docker/containers/ -type f -name "*.log" -size +1G -exec truncate -s 0 {} \;
-
 
 docker builder prune -f
 df -h /
