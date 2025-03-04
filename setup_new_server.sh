bb99dc4b9b4bb1159ce3e893328cc5156e6bb632
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2025-03-04 20:13:59 +0200

    Update setup_new_server.sh

--- setup_new_server.sh
+++ setup_new_server.sh
@@ -12,7 +12,7 @@ bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/add_ssh_
 
 # встановити tmux
 # встановити ncdu
-sudo apt install ncdu tmux htop screen python3-pip -y
+sudo apt install ncdu tmux htop screen python3-pip python3-requests -y
 sudo pip3 install requests
 
 # добавити свап на 8гб оперативки
