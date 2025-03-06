8220235ef33c019495fb77ef2e2da463714fa7b5
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2025-03-06 17:24:30 +0200

    Update create_backup.sh

--- sonaric/create_backup.sh
+++ sonaric/create_backup.sh
@@ -17,16 +17,11 @@ function install_screen {
 install_screen
 
 SCREEN_NAME=sonaric_backup
-LOG_FILE="/var/log/sonaric_backup.log"
-
 cd /root
 screen -ls | grep "$SCREEN_NAME" | awk '{print $1}' | xargs -I{} screen -S {} -X quit # закрити скрін сесію
 
 # echo "restart $SCREEN_NAME" 
 screen -dmS "$SCREEN_NAME" -L
-sleep 1
-screen -S "$SCREEN_NAME" -X logfile "$LOG_FILE"
-screen -S "$SCREEN_NAME" -X colon "logfile flush 0^M"  
 sleep 1
 screen -S "$SCREEN_NAME" -X stuff "sonaric identity-export -o $HOME/.sonaric/identity.file\n"
 sleep 5
@@ -34,8 +29,4 @@ screen -S "$SCREEN_NAME" -X stuff "$pass\n" # press enter
 sleep 5
 screen -S "$SCREEN_NAME" -X stuff "$pass\n" # press enter
 
-
-
-
-
-
+screen -ls | grep "$SCREEN_NAME" | awk '{print $1}' | xargs -I{} screen -S {} -X quit # закрити скрін сесію
