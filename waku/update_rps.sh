885784a829d2489bc57d653290389f6706fd8626
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2025-03-17 18:23:54 +0200

    Update update_rps.sh

--- waku/update_rps.sh
+++ waku/update_rps.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/waku/update_rps.sh) rps_sepolia
 
-RPS=$1
+if [ -n "$1" ]; then
+  RPS="$1"
+else
+  read -p "Enter your RPS: " RPS
+fi
+
 docker-compose -f /root/nwaku-compose/docker-compose.yml down
-sed -i "s|^ETH_CLIENT_ADDRESS=.*|ETH_CLIENT_ADDRESS=${RPS}|" /root/nwaku-compose/.env
+sed -i "s|^RLN_RELAY_ETH_CLIENT_ADDRESS=.*|RLN_RELAY_ETH_CLIENT_ADDRESS=${RPS}|" /root/nwaku-compose/.env
 docker-compose -f /root/nwaku-compose/docker-compose.yml up -d
