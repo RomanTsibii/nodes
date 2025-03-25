977eeea727abe546895163a18d5091c36007995b
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2025-03-26 01:22:19 +0200

    Create deploy.sh

---
+++ Seismic/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/Seismic/deploy.sh) PRIV_KEY
+
+source ~/.bashrc
+cd try-devnet/packages/contract/
+
+if [ -n "$1" ]; then
+  PRIV="$1"
+else
+  read -p "Enter your PRIV_KEY without 0x:" PRIV
+fi
+
+FILE="/root/try-devnet/packages/contract/script/deploy.sh"
+
+# Видаляємо рядок, що містить 'dev_wallet'
+sed -i '/dev_wallet/d' "$FILE"
+sed -i '/prelude/d' "$FILE"
+
+# Замінюємо рядок з privkey=... на privkey=$PRIV
+sed -i "s/^privkey=.*/privkey=$PRIV/" "$FILE"
+
+bash script/deploy.sh
