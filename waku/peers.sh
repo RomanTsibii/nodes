0abcdde64b7a0ac04ebd8189684ca738a2072333
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2025-04-15 14:49:39 +0300

    Update peers.sh

--- waku/peers.sh
+++ waku/peers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/waku/peers.sh)
 
-docker compose -f /root/nwaku-compose/docker-compose.yml logs --tail=30000 | grep totalConnections | tail -1
-
-sleep 10
+peers=$(docker compose -f /root/nwaku-compose/docker-compose.yml logs --tail=30000 | grep totalConnections | tail -1)
+sleep 5
+echo $peers
