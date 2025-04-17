5b98171d0078ef1df6a3e870a039dd27d784ea12
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2025-04-18 00:07:51 +0300

    Create deploy.sh

---
+++ tea/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/tea/deploy.sh) MM_PRIV
+
+# Отримати значення MM_PRIV
+if [ -n "$1" ]; then
+  MM_PRIV="$1"
+else
+  read -p "Enter your MM_PRIV: " MM_PRIV
+fi
+
+# Шлях до конфігураційного файлу
+CONFIG_FILE="/root/my-hardhat-project/hardhat.config.js"
+
+# Екранувати лапки для sed
+ESCAPED_PRIV=$(printf '%s\n' "$MM_PRIV" | sed 's/[\/&]/\\&/g')
+
+# Замінити accounts значення у файлі
+sed -i -E "s/accounts: \[[^]]+\]/accounts: [\"$ESCAPED_PRIV\"]/" "$CONFIG_FILE"
+
+# Запустити деплой
+cd /root/my-hardhat-project && npx hardhat run scripts/deploy.js --network sepolia
