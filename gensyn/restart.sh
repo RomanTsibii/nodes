75833cd3a125171bc78f144101d7a29bbf22f94e
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2025-04-17 13:16:23 +0300

    Create restart.sh

---
+++ gensyn/restart.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/gensyn/restart.sh)
+
+SCREEN_NAME=gensyn
+LOG_FILE="/root/rl-swarm/logs.log"
+
+# зупинити сесія якщо вже існує
+if screen -list | grep -q "\.${SCREEN_NAME}"; then
+   screen -S "$SCREEN_NAME" -X stuff $'\003'
+   sleep 5
+    screen -ls | grep "$SCREEN_NAME" | awk '{print $1}' | xargs -I{} screen -S {} -X quit # закрити скрін сесію
+fi
+
+screen -dmS "$SCREEN_NAME" -L
+screen -S "$SCREEN_NAME" -X logfile "$LOG_FILE"
+screen -S "$SCREEN_NAME" -X colon "logfile flush 0^M" 
+screen -S "$SCREEN_NAME" -X stuff "cd /root/rl-swarm; python3 -m venv .venv; source .venv/bin/activate; ./run_rl_swarm.sh"
+screen -S "$SCREEN_NAME" -X stuff $'\n' # press enter
+
+echo "Install Gensyn"
+MAX_RETRIES=500
+while [ 0 -lt $MAX_RETRIES ]; do
+    # Перевірка, чи існує контейнер "spheronnetwork"
+    if tail -n 1 "$LOG_FILE" | grep -q "Would you like to push models you train in the RL swarm to the Hugging Face Hub?"; then
+      echo "Gensyn успішно запущено."
+      break
+    fi
+    tail 2 $LOG_FILE
+    sleep 5
+    ((counter++))
+done
+screen -S "$SCREEN_NAME" -X stuff $'\n'
+echo "Logs - "
+echo "tail -f /root/rl-swarm/logs.log -n 100"
