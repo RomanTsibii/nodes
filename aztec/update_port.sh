9fd3f683d710a378084e6e8a1af9d399d1ab23e1
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2025-05-22 00:10:16 +0300

    Create update_port.sh

---
+++ aztec/update_port.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/RomanTsibii/nodes/main/aztec/update_port.sh)
+
+docker rm -f aztec-sequencer
+
+sudo iptables -I INPUT -p tcp --dport 18080 -j ACCEPT
+
+docker run -d \
+  --name aztec-sequencer \
+  --restart unless-stopped \
+  --network host \
+  --env-file "$HOME/aztec-sequencer/.evm" \
+  -e DATA_DIRECTORY=/data \
+  -e LOG_LEVEL=debug \
+  -v "$HOME/my-node/node":/data \
+  aztecprotocol/aztec:0.85.0-alpha-testnet.8 \
+  sh -c 'node --no-warnings /usr/src/yarn-project/aztec/dest/bin/index.js \
+    start --network alpha-testnet --node --archiver --sequencer --port 18080'
