6168713d6c9d35216e4d4b135ca39ba5ed490005
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2025-05-27 00:19:04 +0300

    Update update_main.sh

--- massa/update_main.sh
+++ massa/update_main.sh
@@ -10,18 +10,19 @@ if [ ! -d $HOME/massa_backup_main/ ]; then
 fi
 
 # актуальне оновлення
-sudo systemctl stop massad
+sudo systemctl stop massa
 cd $HOME
 rm -rf $HOME/massa
-wget https://github.com/massalabs/massa/releases/download/MAIN.2.4/massa_MAIN.2.4_release_linux.tar.gz 
-tar zxvf massa_MAIN.2.4_release_linux.tar.gz 
-rm massa_MAIN.2.4_release_linux.tar.gz
+
+VERSION="2.5"
+wget "https://github.com/massalabs/massa/releases/download/MAIN.${VERSION}/massa_MAIN.${VERSION}_release_linux.tar.gz" 
+tar -xvf "massa_MAIN.${VERSION}_release_linux.tar.gz" && rm -f "massa_MAIN.${VERSION}_release_linux.tar.gz"
 
 # піднімаємо бекап
 cp -r $HOME/massa_backup_main/wallets/ $HOME/massa/massa-client/
 cp $HOME/massa_backup_main/node_privkey.key $HOME/massa/massa-node/config/node_privkey.key
 
-sudo systemctl start massad
+sudo systemctl start massa
 
 # глянути логи 
 # sudo journalctl -u massad -f
