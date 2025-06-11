3049c2520d5d663e790258e74a568003cd8019a8
Roman Tsibii <43521642+RomanTsibii@users.noreply.github.com>
2025-06-11 23:44:00 +0300

    Update free_memory.sh

--- helper/free_memory.sh
+++ helper/free_memory.sh
@@ -5,16 +5,17 @@
 # видалити логи у докері якщо там більше 1гб
 sudo find /var/lib/docker/containers/ -type f -name "*.log" -size +1G -exec truncate -s 0 {} \;
 
-# remove from docker all images with tag "none"
-docker images -q -a | xargs docker inspect --format='{{.Id}}{{range $rt := .RepoTags}} {{$rt}} {{end}}'|grep -v ':'
-docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+rm -rf /var/log/xrdp.log
+rm -rf /var/log/bot_gaia.log
 
 # removing all logs
 # rm -rf /var/log/*
 rm -rf /var/log/syslog /var/log/syslog.1 /var/log/syslog.2
+# remove from docker all images with tag "none"
+docker images -q -a | xargs docker inspect --format='{{.Id}}{{range $rt := .RepoTags}} {{$rt}} {{end}}'|grep -v ':'
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 
-
-
+rm -rf  /root/storage_0gchain_snapshot.lz4
 sudo journalctl --vacuum-size=100M
 sudo systemctl restart systemd-journald
 docker rm -f nubit
